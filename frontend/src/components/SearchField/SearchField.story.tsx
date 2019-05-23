67552f00aa947d4c094d84a1e98ef1c27e052896
Dana Woodman <dana@danawoodman.com>
2019-05-22 17:46:18 -0700

    Support loading state in search field

--- frontend/src/components/SearchField/SearchField.story.tsx
+++ frontend/src/components/SearchField/SearchField.story.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs'
+import { withKnobs, boolean } from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions'
 import { SearchField } from './SearchField'
 
 storiesOf('components', module)
   .addDecorator(withKnobs)
-  .add('SearchField', () => <SearchField search={action('search')} />)
+  .add('SearchField', () => (
+    <SearchField
+      search={action('search')}
+      searching={boolean('searching', false)}
+    />
+  ))
--- frontend/src/components/SearchField/SearchField.tsx
+++ frontend/src/components/SearchField/SearchField.tsx
@@ -4,9 +4,10 @@ import { Icon } from '../Icon'
 
 export interface Props {
   search: (query: string) => void
+  searching: boolean
 }
 
-export function SearchField({ search, ...props }: Props) {
+export function SearchField({ search, searching = false, ...props }: Props) {
   const input = useRef<HTMLInputElement>(null)
   return (
     <Paper className="px-xs py-xxs df ai-center w-100" elevation={1} {...props}>
@@ -21,14 +22,19 @@ export function SearchField({ search, ...props }: Props) {
         <FormControl className="w-100 df ai-center fd-row">
           <InputBase
             autoFocus
+            disabled={searching}
             className="px-sm py-xs w-100"
             id="input-with-icon-adornment"
             placeholder="Search devices and services..."
             inputRef={input}
           />
 
-          <IconButton className="p-sm" type="submit">
-            <Icon name="search" size="sm" />
+          <IconButton className="p-sm" type="submit" disabled={searching}>
+            <Icon
+              name={searching ? 'spinner-third' : 'search'}
+              spin={searching}
+              size="sm"
+            />
           </IconButton>
         </FormControl>
       </form>
