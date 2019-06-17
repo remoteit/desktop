ae700cc7b81b96ab86e0cb1f6ff111436684d264
Dana Woodman <dana@danawoodman.com>
2019-06-17 13:30:12 -0700

    [DESK-115] Add send feedback link

--- frontend/src/pages/SettingsPage/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage/SettingsPage.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Page } from '../Page'
 import { PageHeading } from '../../components/PageHeading'
 import { SignOutLinkController } from '../../controllers/SignOutLinkController'
+import { Link, Paper } from '@material-ui/core'
+import { Icon } from '../../components/Icon'
 // import styles from './SettingsPage.module.css'
 
 export interface Props {}
@@ -10,9 +12,19 @@ export function SettingsPage({ ...props }: Props) {
   return (
     <Page>
       <PageHeading>Settings</PageHeading>
-      <div className="bg-white bs-1 p-md">
+      <Paper className="p-md">
         <SignOutLinkController />
-      </div>
+        <div className="mt-md">
+          <Link
+            href={encodeURI(
+              `mailto:support@remote.it?subject=Desktop Application Feedback`
+            )}
+          >
+            <Icon name="envelope" className="mr-sm" />
+            Send feedback
+          </Link>
+        </div>
+      </Paper>
     </Page>
   )
 }
