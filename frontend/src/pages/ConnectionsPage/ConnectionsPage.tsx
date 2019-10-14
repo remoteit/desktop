a3e10ed390ecabc1777f99167c29dce9915a7160
jamieruderman <jamie@remote.it>
2019-10-14 11:42:08 -0700

    Sorting connections

--- frontend/src/pages/ConnectionsPage/ConnectionsPage.tsx
+++ frontend/src/pages/ConnectionsPage/ConnectionsPage.tsx
@@ -15,5 +15,6 @@ export const ConnectionsPage = connect(
   mapState,
   mapDispatch
 )(({ connections }: ConnectionsPageProps) => {
+  connections.sort((a: ConnectionInfo) => (a.pid ? -1 : 1))
   return <ConnectionsList connections={connections} />
 })
