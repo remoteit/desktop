ef471ddf913b6f372549699a039dd3373b059cc1
jamieruderman <jamie@remote.it>
2019-12-22 08:45:21 -0800

    Include device name in connecton names

--- frontend/src/components/NameSetting/NameSetting.tsx
+++ frontend/src/components/NameSetting/NameSetting.tsx
@@ -5,7 +5,7 @@ import { newConnection, setConnection } from '../../helpers/connectionHelper'
 
 export const NameSetting: React.FC<{ service: IService; connection?: IConnection }> = ({ service, connection }) => {
   if (!service) return null
-  if (!connection) connection = newConnection(service, {})
+  if (!connection) connection = newConnection(service)
 
   const currentName = (connection && connection.name) || (service && service.name)
 
--- frontend/src/helpers/connectionHelper.ts
+++ frontend/src/helpers/connectionHelper.ts
@@ -4,16 +4,26 @@ import { IService } from 'remote.it'
 import { store } from '../store'
 
 export function newConnection(service?: IService | null, data = {}) {
-  const { user } = store.getState().auth
-  return {
+  const { auth, devices } = store.getState()
+
+  let connection = {
     host: IP_PRIVATE,
     restriction: IP_OPEN,
-    owner: user ? user.username : 'Unknown',
-    name: service ? service.name : 'Unknown',
-    id: service ? service.id : 'Error',
-    deviceID: service ? service.deviceID : 'Unknown',
-    ...data,
-  } as IConnection
+    owner: auth.user ? auth.user.username : 'Unknown',
+    name: 'Unknown',
+    id: 'Error',
+    deviceID: 'Unknown',
+  }
+
+  if (service) {
+    const device = devices.all.find(d => d.id === service.deviceID)
+    connection.name = service.name
+    connection.id = service.id
+    connection.deviceID = service.deviceID
+    if (device) connection.name = `${device.name} - ${service.name}`
+  }
+
+  return { ...connection, ...data } as IConnection
 }
 
 export function setConnection(connection: IConnection) {
