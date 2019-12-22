63fd14dc6ba515d86c66d42b2550c047e2d54c74
jamieruderman <jamie@remote.it>
2019-12-22 09:11:42 -0800

    Don't allow ports already assigned even before checking if in use

--- backend/src/ConnectionPool.ts
+++ backend/src/ConnectionPool.ts
@@ -121,7 +121,7 @@ export default class ConnectionPool {
   private assignPort = async (connection: Connection) => {
     const { port } = connection.params
     if (port) {
-      if (!(await PortScanner.isPortFree(port))) {
+      if (this.usedPorts.includes(port) || !(await PortScanner.isPortFree(port))) {
         connection.params.error = { message: `Port ${port} is in use. Port auto-assigned` }
         connection.params.port = await this.nextFreePort()
       }
