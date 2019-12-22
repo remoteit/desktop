d624f10a9ffaa40b4c9e7b9c34bfba609e89d870
jamieruderman <jamie@remote.it>
2019-12-22 08:44:19 -0800

    Clearing connection on connection details page returns you to connections list

--- frontend/src/buttons/ForgetButton/ForgetButton.tsx
+++ frontend/src/buttons/ForgetButton/ForgetButton.tsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import Controller from '../../services/Controller'
+import { REGEX_FIRST_PATH } from '../../constants'
+import { useLocation, useHistory } from 'react-router-dom'
 import { Tooltip, IconButton } from '@material-ui/core'
 import { Icon } from '../../components/Icon'
 
-type ForgetButtonProps = {
+type Props = {
   connection?: IConnection
   disabled?: boolean
 }
 
-export const ForgetButton: React.FC<ForgetButtonProps> = ({ disabled = false, connection }) => {
+export const ForgetButton: React.FC<Props> = ({ disabled = false, connection }) => {
+  const history = useHistory()
+  const location = useLocation()
+
   if (!connection || connection.active) return null
+
+  const forget = () => {
+    Controller.emit('service/forget', connection)
+    const menu = location.pathname.match(REGEX_FIRST_PATH)
+    const name = menu && menu[0] === '/connections' && history.push('/connections')
+  }
+
   return (
     <Tooltip title="Clear this connection">
-      <IconButton disabled={disabled} onClick={() => Controller.emit('service/forget', connection)}>
+      <IconButton disabled={disabled} onClick={forget}>
         <Icon name="times" size="md" fixedWidth />
       </IconButton>
     </Tooltip>
