c082479e83b7ab336b6ecb379610ebe35681782d
jamieruderman <jamie@remote.it>
2020-01-06 14:29:37 -0800

    Connection log style cleanup

--- frontend/src/components/ConnectionLog/ConnectionLog.tsx
+++ frontend/src/components/ConnectionLog/ConnectionLog.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { ListItemIcon, ListItemText, Divider } from '@material-ui/core'
+import { ListItemIcon, ListItemText, ListItemSecondaryAction, List, Divider, Typography } from '@material-ui/core'
 import { ApplicationState } from '../../store'
 import { ListItemLocation } from '../ListItemLocation'
 import { useLocation } from 'react-router-dom'
+import { colors } from '../../styling'
 import { Icon } from '../Icon'
 
 type Props = {
@@ -18,12 +19,19 @@ export const ConnectionLog: React.FC<Props> = ({ connection }) => {
 
   return (
     <>
-      <ListItemLocation disabled={disabled} pathname={location.pathname + '/log'}>
-        <ListItemIcon>
-          <Icon name="stream" color={disabled ? 'gray' : 'primary'} size="lg" />
-        </ListItemIcon>
-        <ListItemText primary="Raw Connection Log" />
-      </ListItemLocation>
+      <List>
+        <ListItemLocation disabled={disabled} pathname={location.pathname + '/log'}>
+          <ListItemIcon>
+            <Icon name="stream" color={disabled ? 'gray' : 'primary'} size="lg" />
+          </ListItemIcon>
+          <ListItemText primary="Raw Connection Log" />
+        </ListItemLocation>
+        {disabled && (
+          <ListItemSecondaryAction>
+            <Typography variant="caption">EMPTY</Typography>
+          </ListItemSecondaryAction>
+        )}
+      </List>
       <Divider />
     </>
   )
