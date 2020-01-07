17681ef7ab935707ce84d790fa56f18013a99520
jamieruderman <jamie@remote.it>
2020-01-07 14:34:17 -0800

    Better airbrake details

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,13 +19,13 @@ Logger.info('Desktop starting up!')
 process
   .on('uncaughtException', (error: Error) => {
     d('Caught exception', error)
-    AirBrake.notify(error)
-    Logger.warn('CAUGHT EXCEPTION', { error })
+    AirBrake.notify({ message: 'CAUGHT EXCEPTION', error })
+    Logger.warn('CAUGHT EXCEPTION', { error, details: error.toString() })
   })
   .on('unhandledRejection', (reason: Error | any, promise: Promise<any>) => {
     d('Caught exception', reason, promise)
-    AirBrake.notify({ reason, promise })
-    Logger.warn('UNHANDLED PROMISE REJECTION', { reason, promise })
+    AirBrake.notify({ message: 'UNHANDLED PROMISE REJECTION', reason, promise })
+    Logger.warn('UNHANDLED PROMISE REJECTION', { reason, details: reason.toString(), promise })
   })
 
 export const application = new Application()
