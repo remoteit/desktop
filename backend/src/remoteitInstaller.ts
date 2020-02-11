54a0dbfb4f0139bc42ae3f937cbbba15e452a879
jamieruderman <jamie@remote.it>
2020-02-11 14:37:19 -0800

    Remoteit cli version bump to support windows user level file location

--- backend/src/remoteitInstaller.ts
+++ backend/src/remoteitInstaller.ts
@@ -3,6 +3,6 @@ import Installer from './Installer'
 export default new Installer({
   name: 'remoteit',
   repoName: 'remoteit/cli',
-  version: '0.37.3',
+  version: '0.37.4',
   dependencies: ['connectd', 'muxer', 'demuxer'],
 })
