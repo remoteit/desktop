e2d01a95fc0a666e2188cb26093c604dc048eb6f
jamieruderman <jamie@remote.it>
2020-02-22 14:01:34 -0800

    Code review update

--- backend/src/systemInfo.ts
+++ backend/src/systemInfo.ts
@@ -3,16 +3,14 @@ import cli from './cliInterface'
 import lan from './LAN'
 import os from 'os'
 
-export default async function systemInfo() {
+export default async function systemInfo<ILookup>() {
   await lan.getInterfaces()
 
-  let info: ILookup = {
+  return {
     arch: os.arch(),
     platform: environment.platform,
     privateIP: lan.privateIP,
     name: cli.data.device.name,
     interfaces: lan.interfaces,
   }
-
-  return info
 }
