09e8472c22dd3d2e0eaea2165d98d36a52a7cafa
jamieruderman <jamie@remote.it>
2020-03-09 18:45:25 -0700

    Test files cleanup and verification

--- backend/__mocks__/sudo-prompt.ts
+++ backend/__mocks__/sudo-prompt.ts
@@ -5,14 +5,3 @@ export function exec(command: string, options: {}, callback: (error: string, std
 export default {
   exec,
 }
-
-/* 
-const help = jest.fn(
-  (command: string, options: any, callback: (error: Error, stdout: any, stderr: any) => void) => {
-    console.log('HELP CALLED')
-    callback(new Error(), `sudo ${command} executed`, '')
-  }
-)
-const execSpy = jest.spyOn(sudoPrompt, 'exec').mockImplementation(help)
-console.log(help.mock.calls)
-*/
--- backend/__mocks__/x-environment.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-export const isWindows = jest.fn(() => false)
-
-export default {
-  userPath: '../jest/user',
-  adminPath: '../jest/admin',
-  binPath: '../jest/bin',
-  isWindows,
-}
