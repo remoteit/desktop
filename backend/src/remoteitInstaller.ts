46a8b99f1b929056681548b9cd78a49da9e0f2e0
jamieruderman <jamie@remote.it>
2020-03-19 11:31:11 -0700

    Upgrade cli to v1.0.1

--- backend/src/remoteitInstaller.ts
+++ backend/src/remoteitInstaller.ts
@@ -3,6 +3,6 @@ import Installer from './Installer'
 export default new Installer({
   name: 'remoteit',
   repoName: 'remoteit/cli',
-  version: '0.37.6',
+  version: '1.0.1',
   dependencies: ['connectd', 'muxer', 'demuxer'],
 })
