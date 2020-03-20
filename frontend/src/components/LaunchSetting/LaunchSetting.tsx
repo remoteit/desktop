37d57735871b510b05bbbb297649f54c0f1a23fb
jamieruderman <jamie@remote.it>
2020-03-20 13:58:32 -0700

    Adding conditional username token

--- frontend/src/components/LaunchSetting/LaunchSetting.tsx
+++ frontend/src/components/LaunchSetting/LaunchSetting.tsx
@@ -4,15 +4,17 @@ import { InlineSetting } from '../InlineSetting'
 import { useApplication } from '../../services/applications'
 import { newConnection, setConnection } from '../../helpers/connectionHelper'
 
+const SSH_TYPE = 28
+
 export const LaunchSetting: React.FC<{ service: IService; connection?: IConnection }> = ({ service, connection }) => {
   const app = useApplication(service.typeID)
+  const tokens = '[host] [port] [id]' + (service.typeID === SSH_TYPE ? ' [username]' : '')
   if (!connection) connection = newConnection(service)
   let currentLaunchUrl = (connection && connection.launchUrl) || app.launchUrl
-
   return (
     <InlineSetting
       icon="question-circle"
-      iconTooltip="Replacement Tokens [host] [port] [username] [id]"
+      iconTooltip={`Replacement Tokens ${tokens}`}
       value={currentLaunchUrl}
       displayValue={app.parse(currentLaunchUrl, connection)}
       label="Launch URL"
