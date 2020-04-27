4e7367f5fdd2dda4308547f899e3568158a07de4
Evan Bowers <evan@remote.it>
2020-04-27 10:01:11 -0700

    remove get interfaces

--- backend/src/Application.ts
+++ backend/src/Application.ts
@@ -23,7 +23,6 @@ export default class Application {
   }
 
   async constructorSync() {
-    await lan.getInterfaces()
     await lan.checkOob()
     this.bindExitHandlers()
     await environment.setElevatedState()
