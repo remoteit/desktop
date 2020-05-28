19fda9d1edea28ada761d1d48a4bacf90a08fc6a
Jamie Ruderman <jamie@remote.it>
2020-05-28 08:29:47 -0700

    [DESK-576][DESK-575] port changed (#150)
    * Don't re-assign port number when in use
    
    * DESK-575 adding scrollbars and clear to logs

--- frontend/src/components/Body/Body.tsx
+++ frontend/src/components/Body/Body.tsx
@@ -3,14 +3,17 @@ import classnames from 'classnames'
 import { spacing } from '../../styling'
 import { makeStyles } from '@material-ui/core/styles'
 
-export const Body: React.FC<{ inset?: boolean; center?: boolean; className?: string }> = ({
-  inset,
-  center,
-  className = '',
-  children,
-}) => {
+type Props = { inset?: boolean; center?: boolean; scrollbars?: boolean; className?: string }
+
+export const Body: React.FC<Props> = ({ inset, center, scrollbars, className = '', children }) => {
   const css = useStyles()
-  className = classnames(className, css.body, center && css.center, inset && css.inset)
+  className = classnames(
+    className,
+    css.body,
+    center && css.center,
+    inset && css.inset,
+    scrollbars ? css.showScroll : css.hideScroll
+  )
   return <div className={className}>{children}</div>
 }
 
@@ -20,7 +23,6 @@ const useStyles = makeStyles({
     flexGrow: 1,
     position: 'relative',
     '-webkit-overflow-scrolling': 'touch',
-    '&::-webkit-scrollbar': { display: 'none' },
     '& section': {
       padding: `${spacing.xl}px`,
     },
@@ -35,5 +37,17 @@ const useStyles = makeStyles({
     flexDirection: 'column',
     height: '100%',
     padding: `${spacing.md}px ${spacing.md}px`,
+  },
+  hideScroll: {
+    '&::-webkit-scrollbar': { display: 'none' },
+  },
+  showScroll: {
+    '&::-webkit-scrollbar': { '-webkit-appearance': 'none' },
+    '&::-webkit-scrollbar:vertical': { width: 11 },
+    '&::-webkit-scrollbar-thumb': {
+      borderRadius: 8,
+      border: '2px solid white' /* should match background, can't be transparent */,
+      backgroundColor: 'rgba(0, 0, 0, .5)',
+    },
   },
 })
--- frontend/src/components/Container/Container.tsx
+++ frontend/src/components/Container/Container.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import classnames from 'classnames'
 import { makeStyles } from '@material-ui/core/styles'
 import { Body } from '../Body'
 
-export const Container: React.FC<{ header: any; inset?: boolean }> = ({ header, inset, children }) => {
-  const css = useStyles()
+type Props = { header: any; inset?: boolean; scrollbars?: boolean }
 
+export const Container: React.FC<Props> = ({ header, inset, scrollbars, children }) => {
+  const css = useStyles()
   return (
     <div className={css.container}>
       <div>{header}</div>
-      <Body inset={inset}>{children}</Body>
+      <Body inset={inset} scrollbars={scrollbars}>
+        {children}
+      </Body>
     </div>
   )
 }
