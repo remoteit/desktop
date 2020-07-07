25d8d3431516d5ede903bfa9358924c428dd475c
matias <matiaskochman@gmail.com>
2020-07-07 10:07:58 -0300

    added useEffect for updating the username

--- frontend/src/buttons/LaunchButton/LaunchButton.tsx
+++ frontend/src/buttons/LaunchButton/LaunchButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useApplication } from '../../shared/applications'
 import { setConnection } from '../../helpers/connectionHelper'
 import {
@@ -24,6 +24,14 @@ export const LaunchButton: React.FC<Props> = ({ connection, service }) => {
   const app = useApplication(service && service.typeID)
   const [open, setOpen] = useState<boolean>(false)
   const [username, setUsername] = useState<string>((connection && connection.username) || '')
+
+  useEffect(() => {
+    if(connection && connection.username) {
+      setUsername(connection.username)
+    } else {
+      setUsername('');
+    }
+  },[connection?.username])
 
   if (!connection || !connection.active || !app) return null
 
