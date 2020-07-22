763a21e98e967cd5852097e72a1078dcd9d37837
jamieruderman <jamie@remote.it>
2020-07-22 10:12:12 -0700

    CLI v1.4.7

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -12,7 +12,7 @@ export const AIRBRAKE_PROJECT_ID = 223457
 export const AIRBRAKE_PROJECT_KEY = process.env.AIRBRAKE_PROJECT_KEY || 'e1376551dbe5b1326f98edd78b6247ba'
 
 // CLI
-export const CLI_VERSION = '1.4.1'
+export const CLI_VERSION = '1.4.7'
 export const CLI_DOWNLOAD: 'AWS' | 'GitHub' = 'AWS' // AWS or GitHub
 
 // CLI product tracking codes
