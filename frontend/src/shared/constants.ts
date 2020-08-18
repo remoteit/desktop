ef87850132eebe9869b23239ecc8a54f20b33187
Mauricio Villalba <mauricio@remote.it>
2020-08-18 09:53:31 -0300

    Reverse API url

--- frontend/src/shared/constants.ts
+++ frontend/src/shared/constants.ts
@@ -6,7 +6,7 @@
 */
 
 export const API_URL = process.env.API_URL || 'https://api.remot3.it/apv/v27'
-export const GRAPHQL_API = process.env.GRAPHQL_API || 'https://api.remote.it/beta/graphql'
+export const GRAPHQL_API = process.env.GRAPHQL_API || 'https://api.remote.it/v1/graphql'
 export const GRAPHQL_BETA_API = process.env.GRAPHQL_BETA_API || 'https://api.remote.it/beta/graphql'
 export const DEVELOPER_KEY = process.env.DEVELOPER_KEY || 'Mjc5REIzQUQtMTQyRC00NTcxLTlGRDktMTVGNzVGNDYxQkE3'
 
