86f8b7a023f96c0efa0c4bc54c87c1a31eb5af65
Jamie Ruderman <jamie@remote.it>
2020-08-26 18:35:07 -0700

    Patch (#218)
    * [DESK-359] win installer update (#214)
    
    * Installer script update with cli service install and file rename
    
    * Ensure sign in to CLI
    
    * CLI v1.5.9 windows
    
    * CLI v1.6.0
    
    * In app installer should start the system services on install still
    
    * CLI 1.6.0 api changes
    
    * Fixing error handling of CLI error messages
    
    * Signin with authhash validation instead of admin
    
    * Mini connection state error detection update
    
    * Do not require a cli update if cli is current and destkop was updated
    
    * Adding wait for processes to exit before cleanup on uninstall script for windows
    
    * CLI v1.6.1
    
    * 2.7.4
    
    * Fixing unit tests

--- backend/src/CLI.ts
+++ backend/src/CLI.ts
@@ -31,9 +31,10 @@ type IExec = {
 
 type IConnectionStatus = {
   id?: string
-  connectionState?: 'offline' | 'connecting' | 'connected'
+  state?: 'offline' | 'connecting' | 'connected'
   isFailover?: boolean
   isP2P?: boolean
+  error?: ISimpleError
 }
 
 export default class CLI {
@@ -57,8 +58,8 @@ export default class CLI {
     this.read()
   }
 
-  checkSignIn() {
-    if (this.isSignedOut()) this.signIn()
+  async checkSignIn() {
+    if (this.isSignedOut()) await this.signIn()
   }
 
   isSignedOut() {
@@ -132,10 +133,11 @@ export default class CLI {
     this.data.connections = this.data.connections.map(c => {
       const status = json?.connections?.find(s => s.id === c.id)
       if (status) {
-        c.active = status.connectionState === 'connected'
-        c.connecting = status.connectionState === 'connecting'
+        c.active = status.state === 'connected'
+        c.connecting = status.state === 'connecting'
         c.isP2P = status.isP2P
-        d('UPDATE STATUS', { c, status: status.connectionState })
+        c.error = status.error // Can add back when CLI is more careful about creating errors
+        Logger.info('UPDATE STATUS', { c, status: status.state })
       }
       return c
     })
@@ -211,7 +213,7 @@ export default class CLI {
   }
 
   async signIn() {
-    await this.exec({ cmds: [strings.signIn()], admin: true })
+    await this.exec({ cmds: [strings.signIn()], checkAuthHash: true })
     this.read()
   }
 
