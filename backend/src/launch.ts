90a01533261f2cd8045beec6f9cab7c14392ce07
Mauricio Villalba <mauricio@remote.it>
2020-09-01 11:23:49 -0300

    Remove max size new terminal

--- backend/src/launch.ts
+++ backend/src/launch.ts
@@ -11,23 +11,33 @@ export const openCMDforWindows = (launchApp: string) => {
 
     EventBus.emit(EVENTS.notInstalledPutty, {install: false, loading: true})
     Logger.info("LAUNCH APP", {launchApp})
-    child_process.exec(`wmic product where "Name like '%PuTTy%'" get Name, Version`, (error: any, stdout: any) => {
+    const process = child_process.exec(`DIR /S putty.exe /B`,  {cwd: 'C:\\'});
+
+    process.stdout.on('data', (data: any) => {
+      Logger.info(`stdout: ${data}`)
+      EventBus.emit(EVENTS.notInstalledPutty, {install: false, loading: false})
+      const server = launchApp.split('//')[1].split(':')
+      const command = `start putty.exe -ssh ${server[0]} ${server[1]}`
+      Logger.info(`stdout: ${data.replace('\\putty.exe', '').replace(/\\/g, '\\\\')}`)
+      const cwd = data.replace('\\putty.exe', '').replace(/\\/g, '\\\\').trim()
+      child_process.exec(`${command}`, {cwd}, ((error: any) =>{ 
         error && Logger.error(`error: ${error}`)
-        Logger.info(`RESULT PUTTY`, {stdout})
-        if(stdout === '\r\r\n\r\r\n'){
+      }))
+    });
+    
+    process.stderr.on('data', (data: string) => {
+      Logger.info(`stderr: ${data}`);
+    });
+    
+    process.on('close', (code: any) => {
+      Logger.info(`child process exited with code ${code}`);
+    });
+
+    child_process.exec(`wmic product where "Name like '%PuTTy%'" get Name, Version`, (error: any, stdout: any, stderr: any) => {
+        error && Logger.error(`error: ${error}`)
+        Logger.info(`RESULT PUTTY`, {stdout, stderr})
+        if (stdout === '\r\r\n\r\r\n') {
             EventBus.emit(EVENTS.notInstalledPutty, {install: true, loading: false})
-        } else {
-            EventBus.emit(EVENTS.notInstalledPutty, {install: false, loading: false})
-            const server = launchApp.split('//')[1].split(':')
-            const command = `start /MAX putty.exe -ssh ${server[0]} ${server[1]}`
-            child_process.exec(`${command}`,  {cwd: 'C:\\Program Files\\PuTTY'}, ((error: any) =>{ 
-                error && Logger.error(`error: ${error}`)
-            }))
-            Logger.info("NEW LINES")
-            child_process.exec(`${command}`, {cwd: 'C:\\Program Files (x86)\\PuTTY'}, ((error: any) =>{ 
-                error && Logger.error(`error: ${error}`)
-            }))
-            EventBus.emit(EVENTS.minimizeWindows)
         }
     });
 }
