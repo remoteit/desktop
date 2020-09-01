55ae0c9f29d8c654524376704e8b71098378d7c1
Mauricio Villalba <mauricio@remote.it>
2020-08-31 22:29:32 -0300

    Fix open putty to Foreground

--- backend/src/launch.ts
+++ backend/src/launch.ts
@@ -4,13 +4,14 @@ const child_process = require('child_process')
 
 const EVENTS = {
   notInstalledPutty: 'service/putty/required',
+  minimizeWindows: 'windows/minimize'
 }
 
 export const openCMDforWindows = (launchApp: string) => {
 
     EventBus.emit(EVENTS.notInstalledPutty, {install: false, loading: true})
     Logger.info("LAUNCH APP", {launchApp})
-    child_process.exec(`wmic product where "Name like '%PuTTy%'" get Name, Version`, (error: any, stdout: any, stderr: any) => {
+    child_process.exec(`wmic product where "Name like '%PuTTy%'" get Name, Version`, (error: any, stdout: any) => {
         error && Logger.error(`error: ${error}`)
         Logger.info(`RESULT PUTTY`, {stdout})
         if(stdout === '\r\r\n\r\r\n'){
@@ -18,14 +19,15 @@ export const openCMDforWindows = (launchApp: string) => {
         } else {
             EventBus.emit(EVENTS.notInstalledPutty, {install: false, loading: false})
             const server = launchApp.split('//')[1].split(':')
-            const command = `&& start putty.exe -ssh ${server[0]} ${server[1]}`
-            child_process.exec(`cd "c:\\Program Files\\PuTTY" ${command}`,((error: any) =>{ 
+            const command = `start /MAX putty.exe -ssh ${server[0]} ${server[1]}`
+            child_process.exec(`${command}`,  {cwd: 'C:\\Program Files\\PuTTY'}, ((error: any) =>{ 
                 error && Logger.error(`error: ${error}`)
             }))
             Logger.info("NEW LINES")
-            child_process.exec(`cd "c:\\Program Files (x86)\\PuTTY" ${command}`,((error: any) =>{ 
+            child_process.exec(`${command}`, {cwd: 'C:\\Program Files (x86)\\PuTTY'}, ((error: any) =>{ 
                 error && Logger.error(`error: ${error}`)
             }))
+            EventBus.emit(EVENTS.minimizeWindows)
         }
     });
 }
