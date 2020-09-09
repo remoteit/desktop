a2b8263a1446b9d0bd923cb58b864cbb662b990f
Jamie Ruderman <jamie@remote.it>
2020-09-09 09:37:13 -0700

    Fonts update and scanEnabled feature activated in new ui (#240)

--- frontend/src/buttons/AddFromNetworkButton/AddFromNetworkButton.tsx
+++ frontend/src/buttons/AddFromNetworkButton/AddFromNetworkButton.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { IconButton, Tooltip } from '@material-ui/core'
+import { ApplicationState } from '../../store'
+import { useSelector } from 'react-redux'
 import { Icon } from '../../components/Icon'
 
 export const AddFromNetworkButton: React.FC<{ device?: IDevice; thisDevice?: boolean }> = ({ device, thisDevice }) => {
+  const { scanEnabled } = useSelector((state: ApplicationState) => state.ui)
   const history = useHistory()
 
-  if (!device || !thisDevice) return null
+  if (!device || !thisDevice || !scanEnabled) return null
 
   return (
     <Tooltip title="Scan for Services">
       <IconButton onClick={() => history.push(`/devices/${device.id}/edit/add-service/network`)}>
-        <Icon name="wifi" size="md" type="light" />
+        <Icon name="radar" size="md" type="light" />
       </IconButton>
     </Tooltip>
   )
