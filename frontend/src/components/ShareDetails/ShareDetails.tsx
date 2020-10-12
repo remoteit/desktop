287d089c688a8876ebe1f481a6f7fe182363654a
jamieruderman <jamie@remote.it>
2020-10-12 13:41:39 -0700

    Display connected service in users page

--- frontend/src/components/ShareDetails/ShareDetails.tsx
+++ frontend/src/components/ShareDetails/ShareDetails.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { getPermissions } from '../../helpers/userHelper'
 import { ListItemSecondaryAction, Tooltip, makeStyles } from '@material-ui/core'
+import { ApplicationState } from '../../store'
 import { ServiceMiniState } from '../ServiceMiniState'
+import { useSelector } from 'react-redux'
 import { spacing } from '../../styling'
 import { Icon } from '../Icon'
 
 type Props = {
   user: IUser
   device?: IDevice
+  connected?: boolean
 }
 
-export const ShareDetails: React.FC<Props> = ({ user, device }) => {
+export const ShareDetails: React.FC<Props> = ({ user, device, connected }) => {
   const css = useStyles()
 
   if (!device) return null
@@ -23,7 +26,7 @@ export const ShareDetails: React.FC<Props> = ({ user, device }) => {
           <Icon name="scroll" size="sm" type="regular" color="grayDark" />
         </Tooltip>
       )}
-      {!!services.length && services.map(s => <ServiceMiniState key={s.id} service={s} showConnected={false} />)}
+      {!!services.length && services.map(s => <ServiceMiniState key={s.id} service={s} showConnected={!!connected} />)}
     </ListItemSecondaryAction>
   )
 }
