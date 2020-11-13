e34251af8fb3b9637aff4e5376202966fd30f8ae
jamieruderman <jamie@remote.it>
2020-11-13 08:51:54 -0800

    download script updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ URL_DEMUXER=$( (cat ./backend/src/cli-version.json | grep -Eo '"demuxerUrl"[^,]*
 
 URL_CONNECTD=$( (cat ./backend/src/cli-version.json | grep -Eo '"connectdUrl"[^,]*' | grep -Eo '[^:]*$') | sed -e 's/^.//' -e 's/.$//' -e 's/^.//')
 
-CLI_VERSION=$( (cat ./backend/src/cli-version.json | grep -Eo '"cli"[^,]*' | grep -Eo '[^:]*$') | sed -e 's/^.//' -e 's/.$//' -e 's/^.//')
+CLI=$( (cat ./backend/src/cli-version.json | grep -Eo '"cli"[^,]*' | grep -Eo '[^:]*$') | sed -e 's/^.//' -e 's/.$//' -e 's/^.//')
 
 MUXER=$( (cat ./backend/src/cli-version.json | grep -Eo '"muxer"[^,]*' | grep -Eo '[^:]*$') | sed -e 's/^.//' -e 's/.$//' -e 's/^.//')
 
@@ -18,12 +18,14 @@ CONNECTD=$( (cat ./backend/src/cli-version.json | grep -Eo '"connectd"[^,]*' | g
 
 set -x
 
+rm -r ./bin/*
+
 if [ "$(uname)" = "Darwin" ]; then
 
     mkdir -m 777 ./bin/
 
     #Mac
-    curl -L https://${URL_CLI}${CLI_VERSION}/remoteit_mac-osx_x86_64 --output ./bin/remoteit
+    curl -L https://${URL_CLI}${CLI}/remoteit_mac-osx_x86_64 --output ./bin/remoteit
     curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86_64-osx --output ./bin/connectd
     curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86_64-osx --output ./bin/demuxer
     curl -L https://${URL_MUXER}${MUXER}/muxer.x86_64-osx --output ./bin/muxer
@@ -33,7 +35,7 @@ elif [ "$(uname)" = "Linux" ]; then
     mkdir -m 777 ./bin/
 
     #Linux
-    curl -L https://${URL_CLI}${CLI_VERSION}/remoteit_linux_x86_64 --output ./bin/remoteit
+    curl -L https://${URL_CLI}${CLI}/remoteit_linux_x86_64 --output ./bin/remoteit
     curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86_64-ubuntu16.04 --output ./bin/connectd
     curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86_64-ubuntu16.04 --output ./bin/demuxer
     curl -L https://${URL_MUXER}${MUXER}/muxer.x86_64-ubuntu16.04 --output ./bin/muxer
@@ -44,16 +46,16 @@ else
     mkdir -m 777 ./bin/x86
 
     #32 bits Windows 
-    curl -L https://${URL_CLI}${CLI_VERSION}/remoteit_windows_x86.exe --output ./bin/X86/remoteit.exe 
-    curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86-win.exe --output ./bin/X86/connectd.exe
-    curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86-win.exe --output ./bin/X86/demuxer.exe
-    curl -L https://${URL_MUXER}${MUXER}/muxer.x86-win.exe --output ./bin/X86/muxer.exe
+    curl -L https://${URL_CLI}${CLI}/remoteit_windows_x86.exe --output ./bin/x86/remoteit.exe 
+    curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86-win.exe --output ./bin/x86/connectd.exe
+    curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86-win.exe --output ./bin/x86/demuxer.exe
+    curl -L https://${URL_MUXER}${MUXER}/muxer.x86-win.exe --output ./bin/x86/muxer.exe
 
     #64 bits Windows
-    curl -L https://${URL_CLI}${CLI_VERSION}/remoteit_windows_x86_64.exe --output ./bin/X64/remoteit.exe
-    curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86_64-64.exe --output ./bin/X64/connectd.exe
-    curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86_64-64.exe --output ./bin/X64/demuxer.exe
-    curl -L https://${URL_MUXER}${MUXER}/muxer.x86_64-64.exe --output ./bin/X64/muxer.exe
+    curl -L https://${URL_CLI}${CLI}/remoteit_windows_x86_64.exe --output ./bin/x64/remoteit.exe
+    curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86_64-win.exe --output ./bin/x64/connectd.exe
+    curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86_64-64.exe --output ./bin/x64/demuxer.exe
+    curl -L https://${URL_MUXER}${MUXER}/muxer.x86_64-64.exe --output ./bin/x64/muxer.exe
 
 fi
 
