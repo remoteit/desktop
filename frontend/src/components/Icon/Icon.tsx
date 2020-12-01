96782a48aa0053645c01555a796c4917bd204451
Mauricio Villalba <mauricio@remote.it>
2020-12-01 15:40:19 -0300

    Refactors Icon

--- frontend/src/components/Icon/Icon.tsx
+++ frontend/src/components/Icon/Icon.tsx
@@ -1,7 +1,9 @@
-import classnames from 'classnames'
 import React from 'react'
 import './Icon.css'
 import { fontSizes, colors, spacing, Color, FontSize } from '../../styling'
+import { IconName } from '@fortawesome/pro-light-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Box } from '@material-ui/core'
 
 export interface IconProps {
   className?: string
@@ -19,23 +21,27 @@ export interface IconProps {
 
 export type Ref = HTMLSpanElement
 
-export const Icon = React.forwardRef<Ref, IconProps>(
-  ({ className, color, fixedWidth = false, name, size, spin, type = 'light', inline, inlineLeft, ...props }, ref) => {
-    if (!name) return null
+export function Icon({
+  className,
+  color,
+  fixedWidth = false,
+  name,
+  size,
+  spin,
+  type = 'light',
+  inline,
+  inlineLeft,
+  ...props
+}: IconProps): JSX.Element {
+  const styles: any = {}
+  if (color) styles.color = colors[color]
+  if (inline) styles.marginLeft = size ? fontSizes[size] / 2 : spacing.md
+  if (inlineLeft) styles.marginRight = size ? fontSizes[size] / 2 : spacing.md
+  if (size) styles.fontSize = fontSizes[size]
 
-    const classes = classnames(
-      { 'fa-spin': spin, 'fa-fw': fixedWidth, '': inline },
-      `fa${type[0]}`,
-      `fa-${name}`,
-      className
-    )
-
-    const styles: any = {}
-    if (color) styles.color = colors[color]
-    if (inline) styles.marginLeft = size ? fontSizes[size] / 2 : spacing.md
-    if (inlineLeft) styles.marginRight = size ? fontSizes[size] / 2 : spacing.md
-    if (size) styles.fontSize = fontSizes[size]
-
-    return <span className={classes} {...props} style={styles} ref={ref} />
-  }
-)
+  return (
+    <Box color={color && colors[color]} component="span">
+      <FontAwesomeIcon style={styles} icon={['fal', name as IconName]} {...props} />
+    </Box>
+  )
+}
