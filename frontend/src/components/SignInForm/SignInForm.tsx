27d94b0060ceb2583e10c8ff9346fbfd6137c22f
evanrbowers <evan@remote.it>
2020-12-04 08:44:50 -0800

    DESK-853 Update @remote.it/components & @remote.it/services to latest version. (#325)

--- frontend/src/components/SignInForm/SignInForm.tsx
+++ frontend/src/components/SignInForm/SignInForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { AuthUser } from '@remote.it/types'
+import { AuthUser, CognitoUser } from '@remote.it/types'
 import { useDispatch, useSelector } from 'react-redux'
 import { Dispatch, ApplicationState } from '../../store'
 import { CognitoAuth } from '@remote.it/components'
@@ -7,7 +7,7 @@ import theme from '../../styling/theme'
 
 export function SignInForm() {
   const { signInError, authService } = useSelector((state: ApplicationState) => state.auth)
-  const [successUser, setSuccessUser] = useState<AuthUser>()
+  const [successUser, setSuccessUser] = useState<CognitoUser>()
   const { auth } = useDispatch<Dispatch>()
 
   useEffect(() => {
@@ -17,7 +17,7 @@ export function SignInForm() {
   return (
     <CognitoAuth
       themeOverride={theme}
-      onSignInSuccess={(user: AuthUser) => setSuccessUser(user)}
+      onSignInSuccess={(user: CognitoUser) => setSuccessUser(user)}
       errorMessage={signInError}
       authService={authService}
     />
