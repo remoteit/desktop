a04f2fbeef55b810d9856c6364b41ebfceee86ff
jamieruderman <jamie@remote.it>
2020-12-12 12:01:51 -0800

    Hide device restoration feature if not available

--- frontend/src/components/DeviceSetupItem/DeviceSetupItem.tsx
+++ frontend/src/components/DeviceSetupItem/DeviceSetupItem.tsx
@@ -1,16 +1,7 @@
 import React from 'react'
-import { emit } from '../../services/Controller'
 import { useSelector, useDispatch } from 'react-redux'
 import { ApplicationState, Dispatch } from '../../store'
-import {
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  ListItemSecondaryAction,
-  Link,
-  Button,
-  Typography,
-} from '@material-ui/core'
+import { ListItem, ListItemIcon, ListItemText, ListItemSecondaryAction, Link, Typography } from '@material-ui/core'
 import { ListItemLocation } from '../ListItemLocation'
 import { getOwnDevices } from '../../models/accounts'
 import { attributeName } from '../../shared/nameHelper'
@@ -21,14 +12,19 @@ import { Icon } from '../Icon'
 
 export const DeviceSetupItem: React.FC = () => {
   const { ui } = useDispatch<Dispatch>()
-  const { thisDevice, targetDevice, os, links, restore, restoring } = useSelector((state: ApplicationState) => ({
-    thisDevice: getOwnDevices(state).find(d => d.id === state.backend.device.uid),
-    targetDevice: state.backend.device,
-    os: state.backend.environment.os,
-    links: getLinks(state),
-    restore: state.ui.restore,
-    restoring: state.ui.restoring,
-  }))
+  const { thisDevice, targetDevice, os, links, canRestore, restore, restoring } = useSelector(
+    (state: ApplicationState) => ({
+      thisDevice: getOwnDevices(state).find(d => d.id === state.backend.device.uid),
+      targetDevice: state.backend.device,
+      os: state.backend.environment.os,
+      links: getLinks(state),
+      canRestore:
+        state.devices.total > state.devices.size ||
+        !!getOwnDevices(state).find((d: IDevice) => d.state !== 'active' && !d.shared),
+      restore: state.ui.restore,
+      restoring: state.ui.restoring,
+    })
+  )
 
   if (restoring)
     return (
@@ -60,16 +56,18 @@ export const DeviceSetupItem: React.FC = () => {
         <Icon name="hdd" size="md" type="light" />
       </ListItemIcon>
       <ListItemText primary={title} secondary={subtitle} />
-      <ListItemSecondaryAction>
-        {restore ? (
-          <Typography variant="body2" color="textSecondary">
-            Select a device or
-            <Link onClick={() => ui.set({ restore: false })}>cancel</Link>
-          </Typography>
-        ) : (
-          <Link onClick={() => ui.set({ restore: true })}>Restore Device</Link>
-        )}
-      </ListItemSecondaryAction>
+      {canRestore && (
+        <ListItemSecondaryAction>
+          {restore ? (
+            <Typography variant="body2" color="textSecondary">
+              Select a device or
+              <Link onClick={() => ui.set({ restore: false })}>cancel</Link>
+            </Typography>
+          ) : (
+            <Link onClick={() => ui.set({ restore: true })}>Restore Device</Link>
+          )}
+        </ListItemSecondaryAction>
+      )}
     </ListItemLocation>
   )
 }
