54525542736f4eacae520e2b860aeca04304c106
mauricio0000 <65682530+mauricio0000@users.noreply.github.com>
2021-01-06 21:48:20 -0300

    [DESK-803] icons remoteit components (#312)
    * [DESK-803] icons remoteit components
    
    * Refactors Icon
    
    * Remove icons files and use icon component
    
    * Add fab fontawesonme
    
    * Fix hover font awesome
    
    * Add spin
    
    * Revert changes
    
    * Removed unused icon assets
    
    * support regular and solid
    
    Co-authored-by: jamieruderman <jamie@remote.it>

--- frontend/src/components/Icon/Icon.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import { Icon } from './Icon'
-
-describe('components/Icon', () => {
-  test('should not explode', () => {
-    mount(<Icon name="rocket" />)
-  })
-})
--- frontend/src/components/Icon/Icon.tsx
+++ frontend/src/components/Icon/Icon.tsx
@@ -1,8 +1,13 @@
-import classnames from 'classnames'
 import React from 'react'
-import './Icon.css'
 import { fontSizes, colors, spacing, Color, FontSize } from '../../styling'
+import { fal, IconName, IconPrefix } from '@fortawesome/pro-light-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/pro-regular-svg-icons'
+import { fas } from '@fortawesome/pro-solid-svg-icons'
 
+library.add(fal, fab, far, fas)
 export interface IconProps {
   className?: string
   color?: Color
@@ -19,23 +24,40 @@ export interface IconProps {
 
 export type Ref = HTMLSpanElement
 
-export const Icon = React.forwardRef<Ref, IconProps>(
-  ({ className, color, fixedWidth = false, name, size, spin, type = 'light', inline, inlineLeft, ...props }, ref) => {
-    if (!name) return null
+export function Icon({
+  className,
+  color,
+  fixedWidth = false,
+  name,
+  size,
+  spin,
+  type = 'light',
+  inline,
+  inlineLeft,
+  ...props
+}: IconProps): JSX.Element {
+  const styles: any = {}
+  if (color) styles.color = colors[color]
+  if (inline) styles.marginLeft = size ? fontSizes[size] / 2 : spacing.md
+  if (inlineLeft) styles.marginRight = size ? fontSizes[size] / 2 : spacing.md
+  if (size) styles.fontSize = fontSizes[size]
 
-    const classes = classnames(
-      { 'fa-spin': spin, 'fa-fw': fixedWidth, '': inline },
-      `fa${type[0]}`,
-      `fa-${name}`,
-      className
-    )
+  let fontType: IconPrefix = 'fal'
 
-    const styles: any = {}
-    if (color) styles.color = colors[color]
-    if (inline) styles.marginLeft = size ? fontSizes[size] / 2 : spacing.md
-    if (inlineLeft) styles.marginRight = size ? fontSizes[size] / 2 : spacing.md
-    if (size) styles.fontSize = fontSizes[size]
-
-    return <span className={classes} {...props} style={styles} ref={ref} />
+  switch (type) {
+    case 'brands': {
+      fontType = 'fab'
+      break
+    }
+    case 'regular': {
+      fontType = 'far'
+      break
+    }
+    case 'solid': {
+      fontType = 'fas'
+      break
+    }
   }
-)
+
+  return <FontAwesomeIcon style={styles} icon={[fontType, name as IconName]} {...props} spin={spin} />
+}
