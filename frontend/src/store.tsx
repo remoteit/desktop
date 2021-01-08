ee1d2c8d5f72b92943566fafb6b4f22350f7f10b
Jamie Ruderman <jamie@remote.it>
2021-01-08 09:42:02 -0800

    2.10.0 alpha.0 (#353)
    * Frontend console message and Icon styling bug
    
    * 2.10.0-alpha.0

--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -3,6 +3,18 @@ import { models } from './models/rootModel'
 import { createLogger } from 'redux-logger'
 import immer from '@rematch/immer'
 
+console.log(`
+                                               _               _               _                         
+      S t a r t i n g           _             (_)   _         | |             | |       _                
+    ____  _____  ____    ___  _| |_  _____     _  _| |_     __| | _____   ___ | |  _  _| |_  ___   ____  
+   / ___)| ___ ||    \\  / _ \\(_   _)| ___ |   | |(_   _)   / _  || ___ | /___)| |_/ )(_   _)/ _ \\ |  _ \\ 
+  | |    | ____|| | | || |_| | | |_ | ____| _ | |  | |_   ( (_| || ____||___ ||  _ (   | |_| |_| || |_| |
+  |_|    |_____)|_|_|_| \\___/   \\__)|_____)(_)|_|   \\__)   \\____||_____)(___/ |_| \\_)   \\__)\\___/ |  __/ 
+                                                                                                  |_|    
+  set window.LOG_STATE = true to enable redux state logging
+
+  `)
+
 const logger = createLogger({
   predicate: () => !!(window as any).LOG_STATE,
 })
