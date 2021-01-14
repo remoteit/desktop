d01c14c3022e835aed7e814d1a25ba5dd5f60116
Mauricio Villalba <mauricio@remote.it>
2021-01-14 10:34:06 -0300

    update pr missing files

--- frontend/src/services/Controller.ts
+++ frontend/src/services/Controller.ts
@@ -159,6 +159,9 @@ function getEventHandlers() {
     },
 
     // Connections --- TODO validate we need these three channels
+    'reachable/error': error => {
+      ui.set({ errorMessage: error.message })
+    },
     'service/connected': (msg: ConnectionMessage) => {
       logs.add({ id: msg.connection.id, log: msg.raw })
       backend.updateConnection(msg.connection)
