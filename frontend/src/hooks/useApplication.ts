005c28e864bf0a8843f8f92e5608a8b67b26d51e
jamieruderman <jamie@remote.it>
2021-01-15 09:56:58 -0800

    useApplication endless loop crash fix

--- frontend/src/hooks/useApplication.ts
+++ frontend/src/hooks/useApplication.ts
@@ -1,12 +1,5 @@
-import { useEffect, useState } from 'react'
 import { Application, getApplication } from '../shared/applications'
 
 export const useApplication = (context: Application['context'], service?: IService, connection?: IConnection) => {
-  const [app, setApp] = useState<Application>(getApplication(context, service, connection))
-
-  useEffect(() => {
-    setApp(getApplication(context, service, connection))
-  }, [context, service, connection])
-
-  return app
+  return getApplication(context, service, connection)
 }
