3fc80d1789945f93f79301febbe2ac5392f9167c
jamieruderman <jamie@remote.it>
2021-01-17 15:28:58 -0800

    reachable message update

--- backend/src/CLI.ts
+++ backend/src/CLI.ts
@@ -138,7 +138,10 @@ export default class CLI {
         c.reachable = status.reachable
 
         if (status.reachable === false) {
-          c.error = { message: "This connection didn't detect a running service", code: REACHABLE_ERROR_CODE }
+          c.error = {
+            message: 'remote.it connected, but there is no service running on the remote machine.',
+            code: REACHABLE_ERROR_CODE,
+          }
         } else if (status.reachable === true) {
           if (c.error && c.error.code === REACHABLE_ERROR_CODE) c.error = { code: 0, message: '' }
         }
