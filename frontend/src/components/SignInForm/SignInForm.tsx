5940a30876924068914edc12238135400da4712b
mauricio0000 <65682530+mauricio0000@users.noreply.github.com>
2021-03-02 16:23:10 -0300

    [DESK-748] add remember username auth (#418)

--- frontend/src/components/SignInForm/SignInForm.tsx
+++ frontend/src/components/SignInForm/SignInForm.tsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Dispatch, ApplicationState } from '../../store'
 import { CognitoAuth } from '@remote.it/components'
 import theme from '../../styling/theme'
+import { CHECKBOX_REMEMBER_KEY } from '../../models/auth'
 
 export function SignInForm() {
   const { signInError, authService, localUsername } = useSelector((state: ApplicationState) => state.auth)
@@ -18,6 +19,14 @@ export function SignInForm() {
     if (successUser) auth.handleSignInSuccess(successUser)
   }, [successUser])
 
+  const onClickCheckboxRemember = (checked) => {
+    if (checked) {
+      window.localStorage.setItem(KEY_CHECKBOX_REMEMBER, 'true')
+    } else {
+      window.localStorage.removeItem(KEY_CHECKBOX_REMEMBER)
+    }
+  }
+
   return (
     <CognitoAuth
       themeOverride={theme}
@@ -26,6 +35,8 @@ export function SignInForm() {
       authService={authService}
       hideCaptcha={true}
       inputEmail={localUsername}
+      showCheckboxRemember={true}
+      onClickCheckboxRemember={onClickCheckboxRemember}
     />
   )
 }
