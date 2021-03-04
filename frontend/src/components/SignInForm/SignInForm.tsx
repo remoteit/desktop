7a0d94042260f3a93194035f502c754aebcdc2e1
mauricio0000 <65682530+mauricio0000@users.noreply.github.com>
2021-03-04 19:00:16 -0300

    [DESK-0000] fix sign in remember (#425)

--- frontend/src/components/SignInForm/SignInForm.tsx
+++ frontend/src/components/SignInForm/SignInForm.tsx
@@ -9,21 +9,25 @@ import { CHECKBOX_REMEMBER_KEY } from '../../models/auth'
 export function SignInForm() {
   const { signInError, authService, localUsername } = useSelector((state: ApplicationState) => state.auth)
   const [successUser, setSuccessUser] = useState<CognitoUser>()
+  const [rememberMe, setRememberMe] = useState<boolean>(false)
   const { auth } = useDispatch<Dispatch>()
 
   useEffect(() => {
     auth.getUsernameLocal()
+    const remember = window.localStorage.getItem(CHECKBOX_REMEMBER_KEY)
+    setRememberMe(!!remember)
   }, [])
 
   useEffect(() => {
     if (successUser) auth.handleSignInSuccess(successUser)
   }, [successUser])
 
-  const onClickCheckboxRemember = (checked) => {
+  const onClickCheckboxRemember = checked => {
+    setRememberMe(!!checked)
     if (checked) {
-      window.localStorage.setItem(KEY_CHECKBOX_REMEMBER, 'true')
+      window.localStorage.setItem(CHECKBOX_REMEMBER_KEY, 'true')
     } else {
-      window.localStorage.removeItem(KEY_CHECKBOX_REMEMBER)
+      window.localStorage.removeItem(CHECKBOX_REMEMBER_KEY)
     }
   }
 
@@ -37,6 +41,7 @@ export function SignInForm() {
       inputEmail={localUsername}
       showCheckboxRemember={true}
       onClickCheckboxRemember={onClickCheckboxRemember}
+      checkedCheckboxRemember={rememberMe}
     />
   )
 }
