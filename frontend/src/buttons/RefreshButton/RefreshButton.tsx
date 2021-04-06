db4a68094d0d819a4b0e7b4321e5fca57b4e20f8
Mauricio Villalba <mauricio@remote.it>
2021-04-06 14:12:12 -0300

    [DESK-1055] refresh button logs

--- frontend/src/buttons/RefreshButton/RefreshButton.tsx
+++ frontend/src/buttons/RefreshButton/RefreshButton.tsx
@@ -3,16 +3,27 @@ import { Tooltip, IconButton } from '@material-ui/core'
 import { Dispatch, ApplicationState } from '../../store'
 import { useDispatch, useSelector } from 'react-redux'
 import { Icon } from '../../components/Icon'
+import { useLocation } from 'react-router-dom'
 
 export const RefreshButton: React.FC<{ device?: IDevice }> = ({ device }) => {
-  const { fetching } = useSelector((state: ApplicationState) => state.devices)
-  const { devices, ui } = useDispatch<Dispatch>()
+  const { fetching, maxDate, minDate, from } = useSelector((state: ApplicationState) => ({
+    fetching: state.devices.fetching,
+    maxDate: state.logs.maxDate,
+    minDate: state.logs.minDate,
+    from: state.logs.from,
+  }))
+  const { devices, ui, logs } = useDispatch<Dispatch>()
+  const location = useLocation()
 
   const onClick = async () => {
-    if (device) {
-      devices.fetchSingle({ id: device.id })
+    if (location.pathname.includes('/logs')) {
+      logs.fetchLogs({ id: device?.id || '', from, maxDate, minDate })
     } else {
-      ui.refreshAll()
+      if (device) {
+        devices.fetchSingle({ id: device.id })
+      } else {
+        ui.refreshAll()
+      }
     }
   }
 
