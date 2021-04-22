4dc501e00cbfa17f354d3572e5c62fbd64e738c8
Mauricio Villalba <mauricio@remote.it>
2021-04-22 13:44:07 -0300

    Refactors

--- frontend/src/components/SortServices.tsx
+++ frontend/src/components/SortServices.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { IconButton, Menu, MenuItem } from '@material-ui/core'
+import { Badge, IconButton, Menu, MenuItem } from '@material-ui/core'
 import { Icon } from './Icon'
 import { useDispatch } from 'react-redux'
 import { Dispatch } from '../store'
+import { makeStyles } from '@material-ui/core/styles'
+import { fontSize } from '@remote.it/components/lib/styles/variables'
 
 const options = [
   { value: 0, name: 'Alphabetical(A-Z)' },
@@ -11,12 +13,14 @@ const options = [
   { value: 3, name: 'Creation Date (Oldest)' },
 ]
 
-const ITEM_HEIGHT = 48
-
 export const SortServices: React.FC = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
   const { devices } = useDispatch<Dispatch>()
   const open = Boolean(anchorEl)
+  const css = useStyles()
+  const [icon, setIcon] = React.useState('sort-amount-down')
+  const [dot, setDot] = React.useState(false)
+  const [optionSelected, setOptionSelected] = React.useState(0)
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget)
@@ -27,6 +31,7 @@ export const SortServices: React.FC = () => {
   }
 
   const servicesSort = async (type: number) => {
+    setOptionSelected(type)
     let sortService
     switch (type) {
       case 0:
@@ -44,7 +49,8 @@ export const SortServices: React.FC = () => {
         sortService = (a: IService, b: IService) => (a.createdAt < b.createdAt ? 1 : -1)
         break
     }
-
+    optionSelected === 0 || optionSelected === 2 ? setIcon('sort-amount-down') : setIcon('sort-amount-up')
+    setDot(true)
     if (sortService) {
       devices.set({ sortService })
     }
@@ -54,23 +60,22 @@ export const SortServices: React.FC = () => {
   return (
     <>
       <IconButton aria-label="more" aria-controls="long-menu" aria-haspopup="true" onClick={handleClick}>
-        <Icon name="sort-amount-down" size="md" />
+        {dot ? (
+          <Badge variant="dot" badgeContent={true} color="primary">
+            <Icon name={icon} size="md" />
+          </Badge>
+        ) : (
+          <Icon name={icon} size="md" />
+        )}
       </IconButton>
-      <Menu
-        id="long-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={open}
-        onClose={handleClose}
-        PaperProps={{
-          style: {
-            maxHeight: ITEM_HEIGHT * 4.5,
-            width: '20ch',
-          },
-        }}
-      >
+      <Menu id="long-menu" anchorEl={anchorEl} keepMounted open={open} onClose={handleClose}>
         {options.map((option, index) => (
-          <MenuItem key={index} selected={option.value === 0} onClick={() => servicesSort(option.value)}>
+          <MenuItem
+            key={index}
+            selected={option.value === optionSelected}
+            onClick={() => servicesSort(option.value)}
+            className={css.list}
+          >
             {option.name}
           </MenuItem>
         ))}
@@ -78,3 +83,9 @@ export const SortServices: React.FC = () => {
     </>
   )
 }
+
+const useStyles = makeStyles({
+  list: {
+    fontSize: fontSize.sm,
+  },
+})
