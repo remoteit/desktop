66c7e16018a06dbc82d4d2e40d14170f5f459ff7
jamieruderman <jamie@remote.it>
2021-10-07 13:57:27 -0700

    backendAuthenticated cleanup

--- frontend/src/pages/SettingsPage/SettingsPage.tsx
+++ frontend/src/pages/SettingsPage/SettingsPage.tsx
@@ -18,15 +18,12 @@ import analyticsHelper from '../../helpers/analyticsHelper'
 
 export const SettingsPage: React.FC<{ singlePanel?: boolean }> = ({ singlePanel }) => {
   const css = useStyles()
-  const { backendAuthenticated, billing, preferences, remoteUI, licenseIndicator } = useSelector(
-    (state: ApplicationState) => ({
-      backendAuthenticated: state.auth.backendAuthenticated,
-      billing: !!getRemoteitLicense(state)?.plan?.billing,
-      licenseIndicator: selectLicenseIndicator(state),
-      preferences: state.backend.preferences,
-      remoteUI: isRemoteUI(state),
-    })
-  )
+  const { billing, preferences, remoteUI, licenseIndicator } = useSelector((state: ApplicationState) => ({
+    billing: !!getRemoteitLicense(state)?.plan?.billing,
+    licenseIndicator: selectLicenseIndicator(state),
+    preferences: state.backend.preferences,
+    remoteUI: isRemoteUI(state),
+  }))
 
   useEffect(() => {
     analyticsHelper.page('SettingsPage')
