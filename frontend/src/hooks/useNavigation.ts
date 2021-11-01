c80f6a5f4903fb6b6c65e21eff8547b58e59afb5
Mauricio Villalba <mauriciovillalba@192.168.1.14>
2021-11-01 10:11:52 -0300

    rebase and refactors

--- frontend/src/hooks/useNavigation.ts
+++ frontend/src/hooks/useNavigation.ts
@@ -7,6 +7,7 @@ import { selectConnections } from '../helpers/connectionHelper'
 import { selectAnnouncements } from '../models/announcements'
 import { selectLicenseIndicator } from '../models/licensing'
 import { isRemoteUI } from '../helpers/uiHelper'
+import debounce from 'lodash.debounce'
 
 interface INavigationHook {
   menu: string
@@ -38,24 +39,29 @@ export function useNavigation(): INavigationHook {
     remoteUI: isRemoteUI(state),
   }))
   const [shouldUpdate, setShouldUpdate] = useState<boolean>(true)
+  const [currentUIPayload, setCurrentUIPayload] = useState({})
 
   const match = location.pathname.match(REGEX_FIRST_PATH)
   const menu = match ? match[0] : '/devices'
 
-  useEffect(() => {
-    if (navigation[menu] !== location.pathname) {
-      ui.set({ navigation: { ...navigation, [menu]: location.pathname } })
-    }
-  }, [navigation, location, menu])
 
   useEffect(() => {
+    let newValues: any = {}
     if (
       location?.pathname &&
       location?.pathname !== '/' &&
       shouldUpdate &&
       navigationBack.slice(-1)[0] !== location.pathname
     ) {
-      ui.set({ navigationBack: navigationBack.concat([location?.pathname]), navigationForward: [] })
+      newValues = { navigationBack: navigationBack.concat([location?.pathname]), navigationForward: [] }
+    }
+    if (navigation[menu] !== location.pathname) {
+      newValues = { ...newValues, navigation: { ...navigation, [menu]: location.pathname } }
+    }
+
+    if (JSON.stringify(newValues) != JSON.stringify(currentUIPayload) && (newValues.navigationBack || newValues.navigation)) {
+      setCurrentUIPayload(newValues)
+      ui.set(newValues)
     }
   }, [location?.pathname])
 
