09475d298a10f9e24af1ac9e3b0702af52953d23
jamieruderman <jamie@remote.it>
2021-11-16 14:45:50 -0800

    Fix avatar menu error and create PortalUI component

---
+++ frontend/src/components/PortalUI.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { isPortal } from '../services/Browser'
+
+export const PortalUI: React.FC = ({ children }) => {
+  if (!isPortal()) return null
+
+  return <>{children}</>
+}
