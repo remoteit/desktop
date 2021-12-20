d8bc84bc9922d99a803c3abbf7fae5d079ec594f
mauricio0000 <65682530+mauricio0000@users.noreply.github.com>
2021-12-20 14:39:10 -0300

    [DESK-0000] fix build unix (#598)

--- scripts/installCli.sh
+++ scripts/installCli.sh
@@ -32,31 +32,33 @@ if [ "$(uname)" = "Darwin" ]; then
 
 elif [ "$(uname)" = "Linux" ]; then
     
-    mkdir -m 777 ./bin/
 
     if ["$(uname -m)" = "arm64"]; then
 
         #RPI arm64
-        curl -L https://${URL_CLI}${CLI}/remoteit_linux_arm64 --output ./bin/remoteit
-        curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.aarch64-ubuntu20.04 --output ./bin/connectd
-        curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.aarch64-ubuntu20.04 --output ./bin/demuxer
-        curl -L https://${URL_MUXER}${MUXER}/muxer.aarch64-ubuntu20.04 --output ./bin/muxer
+        mkdir -m 777 ./bin/arm64/
+        curl -L https://${URL_CLI}${CLI}/remoteit_linux_arm64 --output ./bin/rarm64/emoteit
+        curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.aarch64-ubuntu20.04 --output ./bin/arm64/connectd
+        curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.aarch64-ubuntu20.04 --output ./bin/arm64/demuxer
+        curl -L https://${URL_MUXER}${MUXER}/muxer.aarch64-ubuntu20.04 --output ./bin/arm64/muxer
 
     elif ["$(uname -m)" = "armv7l"]; then
 
          #RPI armv7
-        curl -L https://${URL_CLI}${CLI}/remoteit_linux_armv7 --output ./bin/remoteit
-        curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.arm-linaro-pi --output ./bin/connectd
-        curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.arm-linaro-pi --output ./bin/demuxer
-        curl -L https://${URL_MUXER}${MUXER}/muxer.arm-linaro-pi --output ./bin/muxer
+        mkdir -m 777 ./bin/armv7/
+        curl -L https://${URL_CLI}${CLI}/remoteit_linux_armv7 --output ./bin/linux/remoteit
+        curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.arm-linaro-pi --output ./bin/linux/connectd
+        curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.arm-linaro-pi --output ./bin/linux/demuxer
+        curl -L https://${URL_MUXER}${MUXER}/muxer.arm-linaro-pi --output ./bin/linux/muxer
 
     else
 
         #Linux
-        curl -L https://${URL_CLI}${CLI}/remoteit_linux_x86_64 --output ./bin/remoteit
-        curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86_64-etch --output ./bin/connectd
-        curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86_64-etch --output ./bin/demuxer
-        curl -L https://${URL_MUXER}${MUXER}/muxer.x86_64-etch --output ./bin/muxer
+        mkdir -m 777 ./bin/linux/
+        curl -L https://${URL_CLI}${CLI}/remoteit_linux_x86_64 --output ./bin/linux/remoteit
+        curl -L https://${URL_CONNECTD}${CONNECTD}/connectd.x86_64-etch --output ./bin/linux/connectd
+        curl -L https://${URL_DEMUXER}${DEMUXER}/demuxer.x86_64-etch --output ./bin/linux/demuxer
+        curl -L https://${URL_MUXER}${MUXER}/muxer.x86_64-etch --output ./bin/linux/muxer
         
     fi
     
