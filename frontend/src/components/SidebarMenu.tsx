2aef335e8b8bc9e8942190167645bebc9c909b5b
jamieruderman <jamie@remote.it>
2022-03-13 13:25:30 -0700

    Hidden sidebar new components

---
+++ frontend/src/components/SidebarMenu.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { ApplicationState, Dispatch } from '../store'
+import { Drawer } from '@material-ui/core'
+import { Sidebar } from './Sidebar'
+
+export const SidebarMenu: React.FC = () => {
+  const open = useSelector((state: ApplicationState) => state.ui.sidebarMenu)
+  const dispatch = useDispatch<Dispatch>()
+
+  return (
+    <Drawer anchor="left" open={open} onClose={() => dispatch.ui.set({ sidebarMenu: false })}>
+      <Sidebar />
+    </Drawer>
+  )
+}
