c65257bfc2108ca539ed7c2756bdc78c693e4f38
jamieruderman <jamie@remote.it>
2022-04-13 17:19:32 -0700

    Adjust sidebar account select for easier clicking and better hiding

--- frontend/src/components/AccountSelect.tsx
+++ frontend/src/components/AccountSelect.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { makeStyles, Chip, TextField, MenuItem, Divider, TextFieldProps } from '@material-ui/core'
+import React, { useState } from 'react'
+import { makeStyles, Chip, TextField, MenuItem, Divider, ListItem } from '@material-ui/core'
 import { ROLE } from '../models/organization'
 import { ApplicationState, Dispatch } from '../store'
 import { useSelector, useDispatch } from 'react-redux'
@@ -7,9 +7,10 @@ import { getActiveAccountId } from '../models/accounts'
 import { spacing } from '../styling'
 import { useHistory } from 'react-router-dom'
 
-export const AccountSelect: React.FC<TextFieldProps> = props => {
+export const AccountSelect: React.FC = () => {
   const css = useStyles()
   const history = useHistory()
+  const [open, setOpen] = useState<boolean>(false)
   const { accounts, devices, tags } = useDispatch<Dispatch>()
   const { user, options, activeId, orgName } = useSelector((state: ApplicationState) => ({
     user: state.auth.user || { id: '', email: '' },
@@ -23,35 +24,40 @@ export const AccountSelect: React.FC<TextFieldProps> = props => {
   if (options.length < 2) return null
 
   return (
-    <TextField
-      {...props}
-      select
-      label="Organization"
-      value={activeId}
-      className={css.selectMenu}
-      onChange={async event => {
-        const id = event.target.value as string
-        if (id) {
-          await accounts.setActive(id.toString())
-          devices.set({ query: '', searched: false, from: 0 })
-          devices.fetch()
-          tags.fetch()
-        }
-      }}
-    >
-      {options.map(option => (
-        <MenuItem className={css.menu} value={option.id} key={option.id}>
-          {option.name}
-          <Chip label={ROLE[option.role]} size="small" />
-        </MenuItem>
-      ))}
-      <Divider className={css.divider} />
-      <MenuItem onClick={() => history.push('/devices/membership')}>Manage memberships...</MenuItem>
-    </TextField>
+    <ListItem dense className={css.wrapper} button onClick={() => setOpen(!open)}>
+      <TextField
+        select
+        fullWidth
+        onSelect={() => setOpen(false)}
+        SelectProps={{ open }}
+        label="Organization"
+        value={activeId}
+        className={css.selectMenu}
+        onChange={async event => {
+          const id = event.target.value as string
+          if (id) {
+            await accounts.setActive(id.toString())
+            devices.set({ query: '', searched: false, from: 0 })
+            devices.fetch()
+            tags.fetch()
+          }
+        }}
+      >
+        {options.map(option => (
+          <MenuItem className={css.menu} value={option.id} key={option.id}>
+            {option.name}
+            <Chip label={ROLE[option.role]} size="small" />
+          </MenuItem>
+        ))}
+        <Divider className={css.divider} />
+        <MenuItem onClick={() => history.push('/devices/membership')}>Manage memberships...</MenuItem>
+      </TextField>
+    </ListItem>
   )
 }
 
 const useStyles = makeStyles(({ palette }) => ({
+  wrapper: { marginTop: spacing.md },
   selectMenu: { '& .MuiChip-root': { display: 'none' }, marginLeft: spacing.sm },
   menu: {
     display: 'flex',
