322512bd7948ad115320abc100e8795981403686
jamieruderman <jamie@remote.it>
2022-04-19 16:07:24 -0700

    Add personal org to org membership page

--- frontend/src/pages/OrganizationMembershipPage.tsx
+++ frontend/src/pages/OrganizationMembershipPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { ApplicationState, Dispatch } from '../store'
 import {
   Typography,
+  Divider,
   Box,
   Chip,
   List,
@@ -9,30 +10,33 @@ import {
   ListItemText,
   ListItemIcon,
   ListItemSecondaryAction,
-  Tooltip,
-  IconButton,
 } from '@material-ui/core'
 import { useSelector, useDispatch } from 'react-redux'
+import { getRemoteitLicense } from '../models/licensing'
 import { LicenseChip } from '../components/LicenseChip'
+import { IconButton } from '../buttons/IconButton'
 import { Container } from '../components/Container'
 import { Duration } from '../components/Duration'
 import { spacing } from '../styling'
+import { Avatar } from '../components/Avatar'
 import { Title } from '../components/Title'
 import { Body } from '../components/Body'
 import { Icon } from '../components/Icon'
 import analyticsHelper from '../helpers/analyticsHelper'
 
 export const OrganizationMembershipPage: React.FC = () => {
-  const { membership, roles } = useSelector((state: ApplicationState) => ({
+  const { membership, organization, license, email } = useSelector((state: ApplicationState) => ({
     membership: state.accounts.membership,
-    roles: state.organization.roles,
+    organization: state.organization,
+    license: getRemoteitLicense(state),
+    email: state.auth.user?.email || '',
   }))
   const { accounts } = useDispatch<Dispatch>()
 
   useEffect(() => {
     analyticsHelper.page('AccountAccessPage')
   }, [])
-
+  console.log('LICENSE', organization)
   return (
     <Container
       header={
@@ -43,6 +47,24 @@ export const OrganizationMembershipPage: React.FC = () => {
     >
       {membership.length ? (
         <List>
+          {organization && (
+            <>
+              <ListItem key={organization.id}>
+                <ListItemIcon>
+                  <Avatar email={email} size={25} />
+                </ListItemIcon>
+                <ListItemText primary={organization.name} secondary="Your organization" />
+                <ListItemSecondaryAction>
+                  <Chip label={organization.roles.find(r => r.id === 'OWNER')?.name} size="small" />
+                  <Box width={100} display="inline-block" textAlign="right" marginRight={`${spacing.md}px`}>
+                    <LicenseChip license={license?.valid ? 'LICENSED' : 'UNLICENSED'} />
+                  </Box>
+                  <IconButton icon="pencil" title="Edit Organization" to="/account/organization" />
+                </ListItemSecondaryAction>
+              </ListItem>
+              <Divider variant="inset" />
+            </>
+          )}
           {membership.map(m => (
             <ListItem key={m.organization.id}>
               <ListItemIcon>
@@ -58,15 +80,15 @@ export const OrganizationMembershipPage: React.FC = () => {
                 }
               />
               <ListItemSecondaryAction>
-                <Chip label={roles.find(r => r.id === m.roleId)?.name} size="small" />
+                <Chip label={m.organization.roles.find(r => r.id === m.roleId)?.name} size="small" />
                 <Box width={100} display="inline-block" textAlign="right" marginRight={`${spacing.md}px`}>
                   <LicenseChip license={m.license} />
                 </Box>
-                <Tooltip title="Leave Account">
-                  <IconButton onClick={() => accounts.leaveMembership(m.organization.id)}>
-                    <Icon name="sign-out" size="md" fixedWidth />
-                  </IconButton>
-                </Tooltip>
+                <IconButton
+                  icon="sign-out"
+                  title="Leave Account"
+                  onClick={() => accounts.leaveMembership(m.organization.id)}
+                />
               </ListItemSecondaryAction>
             </ListItem>
           ))}
