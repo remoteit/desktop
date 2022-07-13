2f2b04af7c7765743e3eb14efe285d6aa9504d5c
Evan Bowers <evan@remote.it>
2022-07-13 11:04:33 -0700

    DESK-1530 make set password work

--- frontend/src/components/ChangePassword/ChangePassword.tsx
+++ frontend/src/components/ChangePassword/ChangePassword.tsx
@@ -9,11 +9,12 @@ import { spacing } from '../../styling'
 export const ChangePassword = () => {
   const [currentPassword, setCurrentPassword] = useState<string>('')
   const [password, setPassword] = useState<string>('')
+  const [isValid, setValid] = useState<boolean>(false)
   const { auth } = useDispatch<Dispatch>()
   const css = useStyles()
 
   const evaluateCurrentPassword = (e: { target: { value: React.SetStateAction<string> } }) => {
-    setCurrentPassword(e.target.value)
+    setCurrentPassword(e.target.value.toString())
   }
   const updatePassword = (event: { preventDefault: () => void }) => {
     event.preventDefault()
@@ -32,10 +33,18 @@ export const ChangePassword = () => {
             label="Current Password"
             onChange={e => evaluateCurrentPassword(e)}
           />
-          <PasswordStrengthInput onChange={(password: string) => setPassword(password)} />
+          <PasswordStrengthInput onChange={(password: string, isValid: boolean) => {
+            console.log('PASSWORD:')
+            console.log(password)
+            console.log('isvalid:')
+            console.log(isValid)
+            setPassword(password)
+            setValid(isValid)
+          }
+          } />
         </Gutters>
         <Gutters bottom="xl">
-          <Button variant="contained" color="primary" type="submit" size="small">
+          <Button variant="contained" color="primary" type="submit" size="small" disabled={!isValid}>
             Save
           </Button>
           <Button color="default" size="small">
