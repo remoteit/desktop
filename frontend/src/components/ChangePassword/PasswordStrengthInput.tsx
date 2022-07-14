5daca29d3947c304eb111f67c369e422d2e27a8c
evanrbowers <evan@remote.it>
2022-07-14 13:42:51 -0700

    [DESK-1530] make set password work (#719)
    * DESK-1530 make set password work
    
    * DESKTOP-1530 Remove logging

--- frontend/src/components/ChangePassword/PasswordStrengthInput.tsx
+++ frontend/src/components/ChangePassword/PasswordStrengthInput.tsx
@@ -114,7 +114,7 @@ export function PasswordStrengthInput({ onChange }: Props) {
         </div>
       </div>
 
-      <TextField variant="filled" type="password" label="Confirm new password" />
+      <TextField variant="filled" type="password" label="Confirm new password" onChange={e => checkPasswordConfirmation(e)}/>
     </>
   )
 }
