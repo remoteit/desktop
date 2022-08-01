9277258d700693e574ac3b6d71032afda6bd9ea8
jamieruderman <jamie@remote.it>
2022-07-31 18:47:15 -0700

    optimizing translations

--- frontend/src/cognito/i18n.ts
+++ frontend/src/cognito/i18n.ts
@@ -1,15 +1,18 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import enTranslation from './locales/en/translations.json'
+import { initReactI18next } from 'react-i18next'
 
-i18n.use(LanguageDetector).init({
-  fallbackLng: 'en',
-  debug: false,
-  ns: ['cognito'],
-  defaultNS: 'cognito',
-  interpolation: { escapeValue: false },
-  react: { useSuspense: false },
-  resources: { en: { cognito: enTranslation } },
-})
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: false,
+    interpolation: { escapeValue: false },
+    react: { useSuspense: false },
+    resources: { en: { translation: enTranslation } },
+  })
 
 export default i18n
