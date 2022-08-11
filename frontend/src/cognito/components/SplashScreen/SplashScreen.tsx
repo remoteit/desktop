5df6e89fe0cbccf26892f1894f4ce8410e487a04
jamieruderman <jamie@remote.it>
2022-08-11 08:30:55 -0700

    Removing more redundant components from cognito

--- frontend/src/cognito/components/SplashScreen/SplashScreen.tsx
+++ frontend/src/cognito/components/SplashScreen/SplashScreen.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { Box } from '@mui/material'
 import { CoBrandingLogo } from '../CoBrandingLogo'
 import { spacing } from '../../../styling'
-import { Logo } from '../Logo'
+import { Logo } from '../../../components/Logo'
 
 export function SplashScreen(): JSX.Element {
   return (
     <>
-      <Box width="140px" position="absolute" top={spacing.lg}>
+      <Box width={140} position="absolute" top={spacing.lg}>
         <CoBrandingLogo />
       </Box>
-      <Logo width="140px" />
+      <Logo width={140} />
     </>
   )
 }
