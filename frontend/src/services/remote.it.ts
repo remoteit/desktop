c9c905f66a8e3ade256bb4f4a7fa5e3cef4537f1
SONALI PATNAIK <sonalipatnaik@SANKALPs-MacBook-Pro.local>
2022-09-13 23:20:09 +0530

    [DESK-1512] remove remote it module use

--- frontend/src/services/remote.it.ts
+++ frontend/src/services/remote.it.ts
@@ -1,4 +1,3 @@
-// import setup from 'remote.it'
 import { getRestApi } from '../helpers/apiHelper'
 import { API_URL } from '../shared/constants'
 import { store } from '../store'
@@ -6,14 +5,6 @@ import { store } from '../store'
 console.log('API_URL', API_URL)
 console.log('getApiURL()', getRestApi())
 
-// export const r3 = setup(
-//   {
-//     apiURL: getRestApi(),
-//     developerKey: DEVELOPER_KEY,
-//     successURL: window.location.origin,
-//   },
-//   getToken
-// )
 
 export async function getToken(): Promise<string> {
   const { auth } = store.dispatch
