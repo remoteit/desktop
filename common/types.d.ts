3f949a334bef181ff6165019e890c1578e4d927a
jamieruderman <jamie@remote.it>
2022-10-14 14:35:54 -0700

    Release patch to fix device fetching on state change event

--- common/types.d.ts
+++ common/types.d.ts
@@ -463,6 +463,7 @@ declare global {
       typeID: IService['typeID']
       platform: IDevice['targetPlatform']
       deviceId: string
+      deviceCreated: Date
       device?: IDevice
       service?: IService
       connection?: IConnection
