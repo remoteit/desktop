a5040a4fb6e84d9763b915f4620bc55f609caa4a
jamieruderman <jamie@remote.it>
2022-11-28 12:05:40 -0800

    Fix auto closing sidebar drawer

--- frontend/src/components/SidebarMenu.tsx
+++ frontend/src/components/SidebarMenu.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import { ApplicationState, Dispatch } from '../store'
 import { Drawer } from '@mui/material'
 import { Sidebar } from './Sidebar'
 
 export const SidebarMenu: React.FC = () => {
+  const dispatch = useDispatch<Dispatch>()
+  const location = useLocation()
   const { open, layout } = useSelector((state: ApplicationState) => ({
     open: state.ui.sidebarMenu,
     layout: state.ui.layout,
   }))
-  const dispatch = useDispatch<Dispatch>()
+
+  React.useEffect(() => {
+    if (open) dispatch.ui.set({ sidebarMenu: false })
+  }, [location])
 
   return (
     <Drawer anchor="left" open={open} onClose={() => dispatch.ui.set({ sidebarMenu: false })}>
