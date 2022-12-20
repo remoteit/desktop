3bf546fb8608af19ad4e3fb981986463e4658ffc
jamieruderman <jamie@remote.it>
2022-12-19 16:58:11 -0800

    Updating access accordion to display guests and org members separately

--- frontend/src/buttons/AddUserButton.tsx
+++ frontend/src/buttons/AddUserButton.tsx
@@ -3,7 +3,7 @@ import { IconButton, ButtonProps } from './IconButton'
 
 export const AddUserButton: React.FC<ButtonProps> = ({ icon, children, ...props }) => {
   return (
-    <IconButton icon={icon || 'share'} title="Share access" size="md" buttonBaseSize="small" {...props}>
+    <IconButton icon={icon || 'share'} title="Share access" size="md" {...props}>
       {children}
     </IconButton>
   )
