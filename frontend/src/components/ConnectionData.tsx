416109ae3d69f8d8175a07a4af99557463a51c83
jamieruderman <jamie@remote.it>
2023-01-13 12:42:28 -0800

    Missing new component

---
+++ frontend/src/components/ConnectionData.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import { getAttributes } from './Attributes'
+import { ConnectionChecklist } from './ConnectionChecklist'
+import { useApplication } from '../hooks/useApplication'
+import { DataDisplay } from './DataDisplay'
+import { Gutters } from './Gutters'
+
+type Props = {
+  connection?: IConnection
+  service?: IService
+  session?: ISession
+}
+
+export const ConnectionData: React.FC<Props> = ({ connection, service, session }) => {
+  const attributes = getAttributes(['lanShare', 'connection', 'duration', 'location', 'initiatorPlatform'])
+  const app = useApplication(service, connection)
+
+  if (!connection && !session) return null
+
+  return (
+    <Gutters bottom="md">
+      <Box display="flex" flexDirection="row">
+        <DataDisplay
+          attributes={attributes}
+          connection={connection}
+          session={session}
+          application={app}
+          width={100}
+          disablePadding
+        />
+        <ConnectionChecklist connection={connection} />
+      </Box>
+    </Gutters>
+  )
+}
