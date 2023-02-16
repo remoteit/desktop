c99c2b3d8c7beb798df17b4b4c95ef21b9466ba6
jamieruderman <jamie@remote.it>
2023-02-16 15:48:42 -0800

    Update to only include build arch specific binary assets

--- backend/src/environment.ts
+++ backend/src/environment.ts
@@ -58,7 +58,6 @@ export class Environment {
       this.userPath = PATHS.WIN_USER_SETTINGS
       this.adminPath = PATHS.WIN_ADMIN_SETTINGS
       this.binPath = this.isDev ? PATHS.WIN_BINARIES_DEV : PATHS.WIN_BINARIES
-      if (this.isWindowsARM()) this.binPath = path.resolve(this.binPath, '../arm64')
       this.deprecatedBinaries = PATHS.WIN_DEPRECATED_BINARIES
     } else if (this.isMac) {
       this.userPath = PATHS.MAC_USER_SETTINGS
@@ -67,25 +66,11 @@ export class Environment {
       this.deprecatedBinaries = PATHS.MAC_DEPRECATED_BINARIES
       this.symlinkPath = PATHS.MAC_SYMLINKS
     } else {
-      if (this.isPi || this.isPiZero) {
-        this.userPath = PATHS.ARMV7L_USER_SETTINGS
-        this.adminPath = PATHS.ARMV7L_ADMIN_SETTINGS
-        this.binPath = this.isDev ? PATHS.ARMV7L_BINARIES_DEV : PATHS.ARMV7L_BINARIES
-        this.deprecatedBinaries = PATHS.ARMV7L_DEPRECATED_BINARIES
-        this.symlinkPath = PATHS.ARMV7L_SYMLINKS
-      } else if (this.isArmLinux) {
-        this.userPath = PATHS.ARM64_USER_SETTINGS
-        this.adminPath = PATHS.ARM64_ADMIN_SETTINGS
-        this.binPath = this.isDev ? PATHS.ARM64_BINARIES_DEV : PATHS.ARM64_BINARIES
-        this.deprecatedBinaries = PATHS.ARM64_DEPRECATED_BINARIES
-        this.symlinkPath = PATHS.ARM64_SYMLINKS
-      } else {
-        this.userPath = PATHS.LINUX_USER_SETTINGS
-        this.adminPath = PATHS.LINUX_ADMIN_SETTINGS
-        this.binPath = this.isDev ? PATHS.LINUX_BINARIES_DEV : PATHS.LINUX_BINARIES
-        this.deprecatedBinaries = PATHS.LINUX_DEPRECATED_BINARIES
-        this.symlinkPath = PATHS.LINUX_SYMLINKS
-      }
+      this.userPath = PATHS.LINUX_USER_SETTINGS
+      this.adminPath = PATHS.LINUX_ADMIN_SETTINGS
+      this.binPath = this.isDev ? PATHS.LINUX_BINARIES_DEV : PATHS.LINUX_BINARIES
+      this.deprecatedBinaries = PATHS.LINUX_DEPRECATED_BINARIES
+      this.symlinkPath = PATHS.LINUX_SYMLINKS
     }
 
     this.logPath = path.resolve(this.userPath, 'log')
