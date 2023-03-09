0a30243aa630df6e0366e6128bdea80071a7cc3b
jamieruderman <jamie@remote.it>
2023-03-09 15:19:35 -0800

    fix auto update install warning

--- frontend/src/pages/OptionsPage.tsx
+++ frontend/src/pages/OptionsPage.tsx
@@ -12,7 +12,7 @@ import { getOwnDevices } from '../selectors/devices'
 import { isRemoteUI } from '../helpers/uiHelper'
 import { DesktopUI } from '../components/DesktopUI'
 import { Container } from '../components/Container'
-import { isRemote } from '../services/Browser'
+import { isRemote, isWindows } from '../services/Browser'
 import { TestUI } from '../components/TestUI'
 import { Title } from '../components/Title'
 
@@ -143,21 +143,23 @@ export const OptionsPage: React.FC = () => {
                 confirmMessage="This will stop and attempt to re-install the system agent."
                 onClick={() => binaries.install()}
               />
-              <ListItemSetting
-                confirm
-                label="Uninstall"
-                disabled={notOwner}
-                subLabel={
-                  'De-register this device, completely remove all saved data, and uninstall the system agent and command line tools. Do this before removing the application from your system. Can only be done by the device owner.'
-                }
-                icon="trash"
-                confirmTitle="Are you sure?"
-                confirmMessage="You will remove this system as a host, your connections and command line utilities."
-                onClick={() => {
-                  emit('uninstall')
-                  ui.set({ waitMessage: 'uninstalling' })
-                }}
-              />
+              {!isWindows && (
+                <ListItemSetting
+                  confirm
+                  label="Uninstall"
+                  disabled={notOwner}
+                  subLabel={
+                    'De-register this device, completely remove all saved data, and uninstall the system agent and command line tools. Do this before removing the application from your system. Can only be done by the device owner.'
+                  }
+                  icon="trash"
+                  confirmTitle="Are you sure?"
+                  confirmMessage="You will remove this system as a host, your connections and command line utilities."
+                  onClick={() => {
+                    emit('uninstall')
+                    ui.set({ waitMessage: 'uninstalling' })
+                  }}
+                />
+              )}
               <ListItemSetting
                 label="Show support files"
                 subLabel="Will show the folders that contain the application logs and config file."
