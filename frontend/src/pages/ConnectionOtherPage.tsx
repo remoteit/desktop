65a50be1a71ca65086a7a1f94c4f7f2a5843c2e2
jamieruderman <jamie@remote.it>
2023-03-15 18:40:04 -0700

    Adding disconnect function for 'other' connections

--- frontend/src/pages/ConnectionOtherPage.tsx
+++ frontend/src/pages/ConnectionOtherPage.tsx
@@ -1,30 +1,36 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { Typography } from '@mui/material'
 import { selectById } from '../selectors/devices'
+import { Typography } from '@mui/material'
+import { newConnection } from '../helpers/connectionHelper'
 import { ConnectionDetails } from '../components/ConnectionDetails'
-import { ApplicationState } from '../store'
+import { useSelector, useDispatch } from 'react-redux'
+import { ApplicationState, Dispatch } from '../store'
 import { NoConnectionPage } from './NoConnectionPage'
 import { ConnectionData } from '../components/ConnectionData'
+import { DynamicButton } from '../buttons/DynamicButton'
 import { InfoButton } from '../buttons/InfoButton'
 import { Container } from '../components/Container'
 import { Gutters } from '../components/Gutters'
 import { Title } from '../components/Title'
 
 export const ConnectionOtherPage: React.FC = () => {
+  const dispatch = useDispatch<Dispatch>()
+  const [stopping, setStopping] = React.useState<string>()
   const { serviceID, sessionID } = useParams<{ serviceID?: string; sessionID?: string }>()
   const { service, device, connection, session } = useSelector((state: ApplicationState) => {
     const [service, device] = selectById(state, undefined, serviceID)
     return {
       service,
       device,
-      connection: state.connections.all.find(c => c.sessionId === sessionID),
+      connection: state.connections.all.find(c => c.sessionId === sessionID) || newConnection(service),
       session: state.sessions.all.find(s => s.id === sessionID),
     }
   })
 
   if (!session) return <NoConnectionPage />
+
+  const thisStopping = stopping === session.id
 
   return (
     <Container
@@ -32,10 +38,29 @@ export const ConnectionOtherPage: React.FC = () => {
       backgroundColor="primaryHighlight"
       bodyProps={{ verticalOverflow: true }}
       header={
-        <Typography variant="h1" gutterBottom>
-          <Title>{session?.target.name}</Title>
-          {device && <InfoButton device={device} service={service} />}
-        </Typography>
+        <>
+          <Typography variant="h1" gutterBottom>
+            <Title>{session?.target.name}</Title>
+            {device && <InfoButton device={device} service={service} />}
+          </Typography>
+          <Gutters top={null} bottom="lg">
+            <DynamicButton
+              size="large"
+              title="Disconnect"
+              color="primary"
+              icon={thisStopping ? 'spinner-third' : 'pause'}
+              iconType="solid"
+              variant="text"
+              loading={thisStopping}
+              disabled={thisStopping}
+              onClick={async () => {
+                setStopping(session.id)
+                await dispatch.connections.proxyDisconnect({ ...connection, sessionId: session.id })
+              }}
+              fullWidth
+            />
+          </Gutters>
+        </>
       }
     >
       <Gutters top={null} size="md" bottom={null}>
