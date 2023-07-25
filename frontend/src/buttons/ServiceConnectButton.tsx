6534023a23944338e6b1467641dd1a262a24f05b
jamieruderman <jamie@remote.it>
2023-07-25 11:53:46 -0700

    Update the connect to this divice message to remove connection graph when can't connect

--- frontend/src/buttons/ServiceConnectButton.tsx
+++ frontend/src/buttons/ServiceConnectButton.tsx
@@ -1,9 +1,8 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext } from 'react'
 import { ConnectionErrorMessage } from '../components/ConnectionErrorMessage'
-import { useDispatch, useSelector } from 'react-redux'
-import { Dispatch, ApplicationState } from '../store'
+import { useDispatch } from 'react-redux'
+import { Dispatch } from '../store'
 import { DeviceContext } from '../services/Context'
-import { ListItemCopy } from '../components/ListItemCopy'
 import { ComboButton } from './ComboButton'
 import { GuideBubble } from '../components/GuideBubble'
 import { ErrorButton } from '../buttons/ErrorButton'
@@ -13,100 +12,64 @@ import { DesktopUI } from '../components/DesktopUI'
 import { PortalUI } from '../components/PortalUI'
 import { Gutters } from '../components/Gutters'
 import { spacing } from '../styling'
-import { Notice } from '../components/Notice'
-import { Link } from '../components/Link'
 
 export const ServiceConnectButton: React.FC = () => {
-  const { device, service, connection, instance, user } = useContext(DeviceContext)
-  const { connectThisDevice } = useSelector((state: ApplicationState) => state.ui)
+  const { device, service, connection, instance } = useContext(DeviceContext)
   const [showError, setShowError] = useState<boolean>(true)
   const dispatch = useDispatch<Dispatch>()
   const css = useStyles()
 
-  useEffect(() => {
-    dispatch.ui.set({ connectThisDevice: false })
-  }, [device?.id])
-
   if (connection.connectLink) return null
-
-  const thisDevice = device?.thisDevice && instance?.owner.id === user.id
-  const displayThisDevice = thisDevice && !connectThisDevice
 
   return (
     <>
-      {displayThisDevice ? (
-        <>
-          <Notice gutterTop solid severity="info">
-            This service can be connected to from anywhere using Remote.It.
-          </Notice>
-          <Notice gutterTop severity="warning">
-            <Typography variant="body2" gutterBottom>
-              You are on the same device as this service, so you should not connect with Remote.It. Connect directly
-              using the address below:
+      <GuideBubble
+        guide="connectButton"
+        enterDelay={400}
+        placement="left"
+        startDate={new Date('2022-09-20')}
+        queueAfter={device ? 'availableServices' : 'addNetwork'}
+        instructions={
+          <>
+            <Typography variant="h3" gutterBottom>
+              <b>
+                <PortalUI>Starting a connection</PortalUI>
+                <DesktopUI>Connect on demand</DesktopUI>
+              </b>
             </Typography>
-            <ListItemCopy
-              label="Local endpoint"
-              value={`${service?.host || '127.0.0.1'}:${service?.port}`}
-              showBackground
-              alwaysWhite
-              fullWidth
-            />
-            <Typography variant="caption" display="block" marginTop={2} marginBottom={1}>
-              <Link color="grayDark.main" onClick={() => dispatch.ui.set({ connectThisDevice: true })}>
-                Connect anyway, I know what I'm doing.
-              </Link>
-            </Typography>
-          </Notice>
-        </>
-      ) : (
-        <GuideBubble
-          guide="connectButton"
-          enterDelay={400}
-          placement="left"
-          startDate={new Date('2022-09-20')}
-          queueAfter={device ? 'availableServices' : 'addNetwork'}
-          instructions={
-            <>
-              <Typography variant="h3" gutterBottom>
-                <b>
-                  <PortalUI>Starting a connection</PortalUI>
-                  <DesktopUI>Connect on demand</DesktopUI>
-                </b>
+            <PortalUI>
+              <Typography variant="body2" gutterBottom>
+                Create a connection to this service with the button to the right.
               </Typography>
-              <PortalUI>
-                <Typography variant="body2" gutterBottom>
-                  Create a connection to this service with the button to the right.
-                </Typography>
-              </PortalUI>
-              <DesktopUI>
-                <Typography variant="body2" gutterBottom>
-                  Start listening on to this endpoint for network requests. On request, automatically create the
-                  connection and disconnect when idle.
-                </Typography>
-              </DesktopUI>
-              {connection.autoLaunch && (
-                <Typography variant="body2" gutterBottom>
-                  <em>
-                    This connection will launch when connected because the "Auto Launch" configuration toggle is on.
-                  </em>
-                </Typography>
-              )}
-            </>
-          }
-        >
-          <Gutters size="md" className={css.gutters} bottom={null}>
-            <ErrorButton connection={connection} onClick={() => setShowError(!showError)} visible={showError} />
-            <ComboButton
-              size="large"
-              service={service}
-              connection={connection}
-              permissions={instance?.permissions}
-              onClick={() => dispatch.ui.guide({ guide: 'aws', step: 6 })}
-              fullWidth
-            />
-          </Gutters>
-        </GuideBubble>
-      )}
+            </PortalUI>
+            <DesktopUI>
+              <Typography variant="body2" gutterBottom>
+                Start listening on to this endpoint for network requests. On request, automatically create the
+                connection and disconnect when idle.
+              </Typography>
+            </DesktopUI>
+            {connection.autoLaunch && (
+              <Typography variant="body2" gutterBottom>
+                <em>
+                  This connection will launch when connected because the "Auto Launch" configuration toggle is on.
+                </em>
+              </Typography>
+            )}
+          </>
+        }
+      >
+        <Gutters size="md" className={css.gutters} bottom={null}>
+          <ErrorButton connection={connection} onClick={() => setShowError(!showError)} visible={showError} />
+          <ComboButton
+            size="large"
+            service={service}
+            connection={connection}
+            permissions={instance?.permissions}
+            onClick={() => dispatch.ui.guide({ guide: 'aws', step: 6 })}
+            fullWidth
+          />
+        </Gutters>
+      </GuideBubble>
       <ConnectionErrorMessage connection={connection} visible={showError} />
     </>
   )
--- frontend/src/components/ServiceHeaderMenu.tsx
+++ frontend/src/components/ServiceHeaderMenu.tsx
@@ -1,5 +1,7 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { DeviceContext } from '../services/Context'
+import { useDispatch, useSelector } from 'react-redux'
+import { Dispatch, ApplicationState } from '../store'
 import { Title } from './Title'
 import { OutOfBand } from './OutOfBand'
 import { Typography } from '@mui/material'
@@ -7,10 +9,13 @@ import { LicensingNotice } from './LicensingNotice'
 import { DeviceOptionMenu } from './DeviceOptionMenu'
 import { ServiceConnectButton } from '../buttons/ServiceConnectButton'
 import { AddUserButton } from '../buttons/AddUserButton'
+import { ListItemCopy } from '../components/ListItemCopy'
 import { Container } from './Container'
 import { Gutters } from './Gutters'
 import { Diagram } from './Diagram'
+import { Notice } from '../components/Notice'
 import { Color } from '../styling'
+import { Link } from '../components/Link'
 
 type Props = {
   footer?: React.ReactNode
@@ -19,9 +24,18 @@ type Props = {
 }
 
 export const ServiceHeaderMenu: React.FC<Props> = ({ footer, backgroundColor, children }) => {
-  const { device, service } = useContext(DeviceContext)
+  const { connectThisDevice } = useSelector((state: ApplicationState) => state.ui)
+  const { device, service, instance, user } = useContext(DeviceContext)
+  const dispatch = useDispatch<Dispatch>()
+
+  useEffect(() => {
+    dispatch.ui.set({ connectThisDevice: false })
+  }, [device?.id])
 
   if (!service || !device) return null
+
+  const thisDevice = device?.thisDevice && instance?.owner.id === user.id
+  const displayThisDevice = thisDevice && !connectThisDevice
 
   return (
     <Container
@@ -44,12 +58,40 @@ export const ServiceHeaderMenu: React.FC<Props> = ({ footer, backgroundColor, ch
             </Gutters>
           )}
           {service.license === 'UNLICENSED' && <LicensingNotice instance={device} fullWidth />}
-          <Gutters top={null} bottom="lg" size="md">
-            <ServiceConnectButton />
-          </Gutters>
-          <Gutters top="xs" bottom="md">
-            <Diagram />
-          </Gutters>
+          {displayThisDevice ? (
+            <Gutters top={null} bottom="lg" size="xxs">
+              <Notice gutterTop solid severity="info">
+                This service can be connected to from anywhere using Remote.It.
+              </Notice>
+              <Notice gutterTop severity="warning">
+                <Typography variant="body2" gutterBottom>
+                  You are on the same device as this service, so you should not connect with Remote.It. Connect directly
+                  using the address below:
+                </Typography>
+                <ListItemCopy
+                  label="Local endpoint"
+                  value={`${service?.host || '127.0.0.1'}:${service?.port}`}
+                  showBackground
+                  alwaysWhite
+                  fullWidth
+                />
+                <Typography variant="caption" display="block" marginTop={2} marginBottom={1}>
+                  <Link color="grayDark.main" onClick={() => dispatch.ui.set({ connectThisDevice: true })}>
+                    Connect anyway, I know what I'm doing.
+                  </Link>
+                </Typography>
+              </Notice>
+            </Gutters>
+          ) : (
+            <>
+              <Gutters top={null} bottom="lg" size="md">
+                <ServiceConnectButton />
+              </Gutters>
+              <Gutters top="xs" bottom="md">
+                <Diagram />
+              </Gutters>
+            </>
+          )}
         </>
       }
       footer={footer}
