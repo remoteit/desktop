17e76686274dc7e758c11c01cc7795809ba98946
jamieruderman <jamie@remote.it>
2023-11-05 08:57:03 -0800

    Close sidebar when changing org

--- frontend/src/components/SidebarMenu.tsx
+++ frontend/src/components/SidebarMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import { selectOrganization } from '../selectors/organizations'
 import { useSelector, useDispatch } from 'react-redux'
 import { ApplicationState, Dispatch } from '../store'
 import { Drawer } from '@mui/material'
@@ -8,14 +9,15 @@ import { Sidebar } from './Sidebar'
 export const SidebarMenu: React.FC = () => {
   const dispatch = useDispatch<Dispatch>()
   const location = useLocation()
-  const { open, layout } = useSelector((state: ApplicationState) => ({
+  const { activeOrg, open, layout } = useSelector((state: ApplicationState) => ({
+    activeOrg: selectOrganization(state),
     open: state.ui.sidebarMenu,
     layout: state.ui.layout,
   }))
 
   React.useEffect(() => {
     if (open) dispatch.ui.set({ sidebarMenu: false })
-  }, [location])
+  }, [location, activeOrg])
 
   return (
     <Drawer anchor="left" open={open} onClose={() => dispatch.ui.set({ sidebarMenu: false })}>
