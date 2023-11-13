d4d6c2609032c1311aea0dc86074697755046bcf
jamieruderman <jamie@remote.it>
2023-11-13 13:46:40 -0800

    Google and Apple sign in button styling

--- frontend/src/cognito/components/GoogleSignInButton.tsx
+++ frontend/src/cognito/components/GoogleSignInButton.tsx
@@ -7,14 +7,17 @@ export function GoogleSignInButton(props: GoogleSignInButtonProps): JSX.Element 
   return (
     <Button
       {...props}
-      variant="contained"
+      variant="outlined"
       sx={{
-        color: 'white.main',
-        bgcolor: 'grayDarkest.main',
-        padding: 1,
-        '&:hover': { bgcolor: 'black.main' },
+        color: 'grayDarker.main',
+        bgcolor: 'grayLightest.main',
+        padding: 0.5,
+        // '&:hover': { bgcolor: 'grayDarkest.main' },
       }}
     >
+      {/* <Stack height={36} width={40} justifyContent="center">
+        <Icon name="google" type="brands" fontSize={20} />
+      </Stack> */}
       <svg width="40px" height="40px" viewBox="0 0 40 40" version="1.1">
         <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
           <g id="google" transform="translate(11.000000, 11.000000)">
