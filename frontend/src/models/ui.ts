d695eed35373a677a49c463af08ca7a84df0e6e2
jamieruderman <jamie@remote.it>
2024-01-03 13:13:29 -0800

    Set android status bar icon color

--- frontend/src/models/ui.ts
+++ frontend/src/models/ui.ts
@@ -1,5 +1,6 @@
 import structuredClone from '@ungap/structured-clone'
 import { emit } from '../services/Controller'
+import { StatusBar, Style } from '@capacitor/status-bar'
 import { RootModel } from '.'
 import { isDarkMode } from '../styling/theme'
 import { NoticeProps } from '../components/Notice'
@@ -216,8 +217,10 @@ export default createModel<RootModel>()({
     },
     async setTheme(themeMode: UIState['themeMode'] | undefined, state) {
       themeMode = themeMode || state.ui.themeMode
+      const themeDark = isDarkMode(themeMode)
       dispatch.ui.setPersistent({ themeMode })
-      dispatch.ui.set({ themeDark: isDarkMode(themeMode) })
+      dispatch.ui.set({ themeDark })
+      if (browser.isAndroid) StatusBar.setStyle({ style: themeDark ? Style.Dark : Style.Light })
     },
     async resizeColumn(params: { id: string; width: number }, state) {
       const columnWidths = { ...state.ui.columnWidths, [params.id]: params.width }
