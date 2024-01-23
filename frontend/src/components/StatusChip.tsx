cbef73d30858ea677e230068710c28c085c46d4d
jamieruderman <jamie@remote.it>
2024-01-22 17:46:48 -0800

    Offline status should override other status in status chip

--- frontend/src/components/StatusChip.tsx
+++ frontend/src/components/StatusChip.tsx
@@ -11,6 +11,8 @@ export const StatusChip: React.FC<Props> = ({ device, service, connections }) =>
   const instance = device || service
   return instance?.license === 'UNLICENSED' ? (
     <ColorChip label="Unlicensed" size="small" color="warning" />
+  ) : instance?.state === 'inactive' ? (
+    <ColorChip label="Offline" size="small" color="gray" />
   ) : connections?.some(c => c.connected) ? (
     <ColorChip label="Connected" size="small" color="primary" variant="contained" />
   ) : connections?.some(c => c.connectLink) ? (
@@ -20,6 +22,6 @@ export const StatusChip: React.FC<Props> = ({ device, service, connections }) =>
   ) : instance?.state === 'active' ? (
     <ColorChip label="Online" size="small" color="secondary" />
   ) : (
-    <ColorChip label="Offline" size="small" color="gray" />
+    <ColorChip label="Unknown" size="small" color="gray" />
   )
 }
