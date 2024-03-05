5b4d76a26bc54ee6b8f26abd02dbe004d35622c2
jamieruderman <jamie@remote.it>
2024-03-05 11:24:11 -0800

    Improved logging of web console errors, and always sign in CLI when backend signs in

--- backend/src/Logger.ts
+++ backend/src/Logger.ts
@@ -9,12 +9,30 @@ interface TransformableInfoWithSymbol extends TransformableInfo {
   [key: symbol]: any
 }
 
+const safeStringify = (obj: any) => {
+  try {
+    return JSON.stringify(obj, null, 2) // 2-space indentation
+  } catch (error) {
+    return util.inspect(obj, { depth: null, colors: false })
+  }
+}
+
 const logFormat = format.combine(
   format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
   {
     transform: (info: TransformableInfoWithSymbol) => {
       const args = info[Symbol.for('splat')]
-      if (args) info.message = util.format(info.message, ...args)
+      if (args) {
+        for (let i = 0; i < args.length; i++) {
+          if (typeof args[i] === 'object' && !(args[i] instanceof String)) {
+            args[i] = safeStringify(args[i])
+          }
+        }
+        info.message = util
+          .format(info.message, ...args)
+          .replace(/\\n/g, '\n')
+          .replace(/\\'/g, "'")
+      }
       return info
     },
   },
