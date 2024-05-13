9f7f6869857bb3530068ec1e24cc52ad1ac2ae21
jamieruderman <jamie@remote.it>
2024-05-13 15:53:47 -0700

    Fix upgrade banner visibility

--- frontend/src/components/UpgradeBanner.tsx
+++ frontend/src/components/UpgradeBanner.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { selectPlan } from '../selectors/organizations'
 import { PERSONAL_PLAN_ID } from '../models/plans'
@@ -8,9 +8,8 @@ import { Notice } from './Notice'
 import { Box } from '@mui/material'
 
 export const UpgradeBanner: React.FC = () => {
-  const [open, setOpen] = useState<boolean>(true)
   const plan = useSelector(selectPlan)
-  if (plan?.id === PERSONAL_PLAN_ID || !open) return null
+  if (plan?.id !== PERSONAL_PLAN_ID) return null
   return (
     <Notice gutterBottom>
       Access premium features & support
