2f9f57b5abe41d42819248ee73b7bdfcd9358904
jamieruderman <jamie@remote.it>
2024-05-29 13:44:59 -0700

    Change wifi select input to autocomplete with free input

--- frontend/src/components/OnboardWifi.tsx
+++ frontend/src/components/OnboardWifi.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { State, Dispatch } from '../store'
 import { useSelector, useDispatch } from 'react-redux'
 import {
@@ -11,6 +11,7 @@ import {
   ListItemText,
   TextField,
   InputAdornment,
+  Autocomplete,
   MenuItem,
 } from '@mui/material'
 import { OnboardMessage } from './OnboardMessage'
@@ -26,9 +27,10 @@ type Props = {
 export const OnboardWifi: React.FC<Props> = ({ next }) => {
   const dispatch = useDispatch<Dispatch>()
   const { message, severity, ssid, scan, wifi, networks } = useSelector((state: State) => state.bluetooth)
+  const [showPassword, setShowPassword] = useState(false)
   const [ready, setReady] = useState<boolean>(false)
   const [form, setForm] = useState({ ssid: '', pwd: '' })
-  const [showPassword, setShowPassword] = useState(false)
+  const passwordRef = useRef<HTMLInputElement>()
 
   useEffect(() => {
     dispatch.bluetooth.readSSIDs()
@@ -42,7 +44,8 @@ export const OnboardWifi: React.FC<Props> = ({ next }) => {
     if (ready && !message && wifi === 'CONNECTED') next()
   }, [wifi, ready])
 
-  const onScan = async () => {
+  const onScan = async event => {
+    event.stopPropagation()
     await dispatch.bluetooth.scanSSIDs()
     await dispatch.bluetooth.readSSIDs()
   }
@@ -50,7 +53,7 @@ export const OnboardWifi: React.FC<Props> = ({ next }) => {
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     await dispatch.bluetooth.writeWifi(form)
-    setTimeout(() => setReady(true), 1000)
+    setTimeout(() => setReady(true), 500)
   }
 
   const selectedNetwork = networks.find(network => network.ssid === form.ssid)
@@ -70,42 +73,61 @@ export const OnboardWifi: React.FC<Props> = ({ next }) => {
         Select a network and enter its password.
       </Typography>
       <OnboardMessage message={message} severity={severity} />
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} autoComplete="off">
         <List>
-          <TextField
-            select
+          <Autocomplete
+            freeSolo
+            autoFocus
             fullWidth
-            label="Network"
-            variant="filled"
-            value={form.ssid || 'none'}
+            autoComplete
+            selectOnFocus
+            forcePopupIcon
+            disableClearable
+            handleHomeEndKeys
+            includeInputInList
             disabled={disabled}
+            noOptionsText="No networks found"
+            options={networks.map(n => n.ssid) || []}
+            value={disabled ? 'Scanning...' : form.ssid}
             sx={{ marginBottom: 1 }}
-            InputProps={{
-              endAdornment: !disabled && (
-                <InputAdornment position="end" sx={{ position: 'absolute', right: ({ spacing }) => spacing(3.3) }}>
-                  {selectedNetwork && <SignalIcon strength={selectedNetwork.signal} type="solid" />}
-                  <IconButton onClick={onScan} icon="radar" />
-                </InputAdornment>
-              ),
+            onChange={(event, ssid, reason) => {
+              if (!ssid || disabled) return
+              setForm({ ...form, ssid })
+              passwordRef.current?.focus()
             }}
-            SelectProps={{
-              renderValue: value => (scan === 'SCANNING' ? <>Scanning...</> : (value as React.ReactNode)),
-            }}
-            onChange={event => setForm({ ...form, ssid: event.target.value })}
-          >
-            {scan === 'SCANNING' ? (
-              <MenuItem value="none">Scanning...</MenuItem>
-            ) : networks.length ? (
-              networks.map(network => (
-                <MenuItem value={network.ssid} key={network.ssid}>
-                  <ListItemText>{network.ssid}</ListItemText>
-                  <SignalIcon strength={network.signal} type="solid" />
+            renderOption={(props, ssid) => {
+              const network = networks.find(n => n.ssid === ssid)
+              return (
+                <MenuItem {...props} key={ssid}>
+                  <ListItemText>{ssid}</ListItemText>
+                  {network && <SignalIcon strength={network.signal} type="solid" />}
                 </MenuItem>
-              ))
-            ) : (
-              <MenuItem value="none">No networks found</MenuItem>
+              )
+            }}
+            renderInput={params => (
+              <TextField
+                variant="filled"
+                label="Network"
+                {...params}
+                autoComplete="off"
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: !disabled && (
+                    <>
+                      {params.InputProps.endAdornment}
+                      <InputAdornment
+                        position="end"
+                        sx={{ position: 'absolute', marginTop: -2, right: ({ spacing }) => spacing(3.5) }}
+                      >
+                        {selectedNetwork && <SignalIcon strength={selectedNetwork.signal} type="solid" />}
+                        <IconButton onClick={onScan} icon="radar" />
+                      </InputAdornment>
+                    </>
+                  ),
+                }}
+              />
             )}
-          </TextField>
+          />
           <TextField
             fullWidth
             type={showPassword ? 'text' : 'password'}
@@ -113,7 +135,9 @@ export const OnboardWifi: React.FC<Props> = ({ next }) => {
             value={form.pwd}
             variant="filled"
             disabled={disabled}
-            autoComplete="off"
+            autoComplete="new-password"
+            name="wifi-password"
+            inputRef={passwordRef}
             onChange={event => setForm({ ...form, pwd: event.target.value })}
             InputProps={{
               endAdornment: (
