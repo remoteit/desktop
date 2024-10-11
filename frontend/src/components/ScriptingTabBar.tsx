6d1654605b63a0e444fa4985f56e0c101f579930
Jamie Ruderman <jamieruderman@gmail.com>
2024-10-10 17:05:07 -0700

    Remove Assets tab from scripting page for now

--- frontend/src/components/ScriptingTabBar.tsx
+++ frontend/src/components/ScriptingTabBar.tsx
@@ -6,8 +6,8 @@ import { useHistory, useLocation } from 'react-router-dom'
 import { Tabs, Tab } from '@mui/material'
 import { MasterTab } from './MasterTab'
 
-const tabs = ['/runs', '/scripts', '/files']
-const tabTitles = ['Runs', 'Scripts', 'Assets']
+const tabs = ['/scripts', '/runs' /* , '/files' */]
+const tabTitles = ['Scripts', 'Runs', 'Assets']
 
 export const ScriptingTabBar: React.FC = () => {
   const dispatch = useDispatch<Dispatch>()
