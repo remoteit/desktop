e1cc27f86166dd89fccc8a75347743ab5c9e2e6b
Jamie Ruderman <jamieruderman@gmail.com>
2024-11-01 14:55:42 -0700

    Scripting add demo script button

--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -1,6 +1,5 @@
 import React, { useCallback, useState, useEffect } from 'react'
 import { BINARY_DATA_TOKEN } from '../constants'
-import { makeStyles } from '@mui/styles'
 import { containsNonPrintableChars } from '../helpers/utilHelper'
 import { Typography, TextField, Box, ButtonBase, Stack, Divider } from '@mui/material'
 import { useDropzone } from 'react-dropzone'
@@ -8,7 +7,7 @@ import { useDispatch } from 'react-redux'
 import { IconButton } from '../buttons/IconButton'
 import { Dispatch } from '../store'
 import { Notice } from './Notice'
-import { spacing, radius } from '../styling'
+import { radius } from '../styling'
 
 type Props = {
   script?: string
@@ -17,7 +16,7 @@ type Props = {
 }
 
 export const FileUpload: React.FC<Props> = ({ script = '', loading, onChange }) => {
-  const { ui } = useDispatch<Dispatch>()
+  const dispatch = useDispatch<Dispatch>()
   const [filename, setFilename] = useState<string | undefined>()
   const [isText, setIsText] = useState(true)
 
@@ -29,8 +28,8 @@ export const FileUpload: React.FC<Props> = ({ script = '', loading, onChange }) 
     files.forEach(file => {
       const reader = new FileReader()
 
-      reader.onabort = () => ui.set({ errorMessage: 'File reading was aborted' })
-      reader.onerror = () => ui.set({ errorMessage: 'File reading has failed' })
+      reader.onabort = () => dispatch.ui.set({ errorMessage: 'File reading was aborted' })
+      reader.onerror = () => dispatch.ui.set({ errorMessage: 'File reading has failed' })
       reader.onloadend = () => {
         const buffer = new Uint8Array(reader.result as ArrayBuffer)
 
@@ -47,7 +46,7 @@ export const FileUpload: React.FC<Props> = ({ script = '', loading, onChange }) 
           }
         } catch (e) {
           console.error('Error decoding text:', e)
-          ui.set({ errorMessage: 'File could not be decoded as text.' })
+          dispatch.ui.set({ errorMessage: 'File could not be decoded as text.' })
           setIsText(false)
         }
       }
@@ -57,13 +56,17 @@ export const FileUpload: React.FC<Props> = ({ script = '', loading, onChange }) 
   }, [])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
-  const css = useStyles({ isDragActive })
   const showUpload = !filename && isText
 
   const clear = () => {
     setFilename(undefined)
     onChange('')
     setIsText(true)
+  }
+
+  const paste = async () => {
+    const demo = await dispatch.files.downloadDemoScript()
+    onChange(demo + '\n\n' + script)
   }
 
   return (
@@ -92,11 +95,13 @@ export const FileUpload: React.FC<Props> = ({ script = '', loading, onChange }) 
             inputProps={{ sx: { transition: 'height 600ms' } }}
             onChange={event => onChange(event.target.value)}
           />
-          {filename && (
-            <Box sx={{ position: 'absolute', top: 0, right: 0 }}>
+          <Box sx={{ position: 'absolute', top: 4, right: 4 }}>
+            {filename ? (
               <IconButton name="times" title="Clear" color="grayDark" size="sm" onClick={clear} />
-            </Box>
-          )}
+            ) : (
+              <IconButton name="terminal" title="Add Demo Script" color="grayDark" size="sm" onClick={paste} />
+            )}
+          </Box>
         </>
       ) : (
         <Notice onClose={clear} closeTitle="Clear" fullWidth>
@@ -105,32 +110,31 @@ export const FileUpload: React.FC<Props> = ({ script = '', loading, onChange }) 
       )}
       {showUpload && (
         <>
-          <Divider />
-          <ButtonBase className={css.paper} {...getRootProps()}>
+          <Divider sx={{ borderColor: 'grayLight.main' }} />
+          <ButtonBase
+            {...getRootProps()}
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              border: '2px dotted',
+              borderColor: isDragActive ? 'primary.main' : 'grayLightest.main',
+              backgroundColor: 'grayLightest.main',
+              padding: 2,
+              borderRadius: `0 0 ${radius.sm}px ${radius.sm}px`,
+              minWidth: 200,
+              '&:hover': { backgroundColor: 'primaryHighlight.main', borderColor: 'primaryHighlight.main' },
+            }}
+          >
             <input {...getInputProps()} />
-            <Box>
-              <Typography variant="body2">Upload</Typography>
-              <Typography variant="caption">Drag and drop or click </Typography>
-            </Box>
+            {/* <Box> */}
+            <Typography variant="body2">Upload</Typography>
+            <Typography variant="caption">Drag and drop or click </Typography>
+            {/* </Box> */}
           </ButtonBase>
         </>
       )}
     </Stack>
   )
 }
-
-type styleProps = { isDragActive: boolean }
-
-const useStyles = makeStyles(({ palette }) => ({
-  paper: ({ isDragActive }: styleProps) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    border: `2px dotted ${isDragActive ? palette.primary.main : palette.grayLightest.main}`,
-    background: palette.grayLightest.main,
-    padding: `${spacing.md}px ${spacing.xl}px`,
-    borderRadius: `0 0 ${radius.sm}px ${radius.sm}px`,
-    minWidth: 200,
-    '&:hover': { background: palette.primaryHighlight.main, borderColor: palette.primaryHighlight.main },
-  }),
-}))
