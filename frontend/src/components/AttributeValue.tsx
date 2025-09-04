3569c6ca235404380d9ea36207c6768c42673229
Jamie Ruderman <jamieruderman@gmail.com>
2025-09-04 15:22:42 -0700

    support line breaks in attribute values for multi line attributes

--- frontend/src/components/AttributeValue.tsx
+++ frontend/src/components/AttributeValue.tsx
@@ -5,10 +5,29 @@ import { Box } from '@mui/material'
 
 export const AttributeValue: React.FC<IDataOptions & { attribute?: Attribute }> = ({ attribute, ...options }) => {
   const value = attribute?.value({ instance: options.device, ...options }) || ''
+  
+  if (attribute?.multiline) {
+    const lines = value.split('\n')
+    return (
+      <Box
+        className={`attribute attribute-${attribute?.id}`}
+        style={{ whiteSpace: 'pre-line' }}
+        textAlign={attribute?.align}
+        marginRight={attribute?.align === 'right' ? `${spacing.md}px` : undefined}
+      >
+        {lines.map((line, index) => (
+          <React.Fragment key={index}>
+            {line}
+            {index < lines.length - 1 && <br />}
+          </React.Fragment>
+        ))}
+      </Box>
+    )
+  }
+
   return (
     <Box
       className={`attribute attribute-${attribute?.id}`}
-      style={{ whiteSpace: attribute?.multiline ? 'pre-line' : undefined }}
       textAlign={attribute?.align}
       marginRight={attribute?.align === 'right' ? `${spacing.md}px` : undefined}
     >
