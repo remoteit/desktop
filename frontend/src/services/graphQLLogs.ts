a3e7a18ebab34e75e6c81df5f31ff2780a8c2b27
Jamie Ruderman <jamieruderman@gmail.com>
2026-03-10 16:59:41 -0700

    Fetch connection usage separately from logs

--- frontend/src/services/graphQLLogs.ts
+++ frontend/src/services/graphQLLogs.ts
@@ -1,7 +1,7 @@
 import { graphQLBasicRequest } from './graphQL'
 
 const EVENTS = `
-    events(after: $after, size: $size, minDate: $minDate, maxDate: $maxDate) {
+    events(after: $after, size: $size, minDate: $minDate, maxDate: $maxDate, types: $types) {
       hasMore
       last
       total
@@ -38,11 +38,6 @@ const EVENTS = `
             }
           }
         }
-        ... on DeviceConnectEvent {
-          txBytes
-          rxBytes
-          lifetime
-        }
         ... on JobEvent {
           job {
             file {
@@ -53,11 +48,64 @@ const EVENTS = `
       }
     }`
 
-const EVENTS_URL = 'eventsUrl(minDate: $minDate, maxDate: $maxDate)'
+const EVENTS_URL = 'eventsUrl(minDate: $minDate, maxDate: $maxDate, types: $types)'
 
-export async function graphQLGetDeviceLogs(id: string, size: number, after?: string, minDate?: Date, maxDate?: Date) {
+const CONNECTION_USAGE_EVENTS = `
+    events(
+      after: $after
+      size: $size
+      minDate: $minDate
+      maxDate: $maxDate
+      types: [DEVICE_CONNECT]
+    ) {
+      items {
+        timestamp
+        state
+        actor {
+          email
+        }
+        target {
+          id
+        }
+        ... on DeviceConnectEvent {
+          session
+          txBytes
+          rxBytes
+          lifetime
+        }
+      }
+    }`
+
+const DEVICE_CONNECTION_USAGE_EVENTS = `
+    events(
+      after: $after
+      size: $size
+      minDate: $minDate
+      maxDate: $maxDate
+      types: [DEVICE_CONNECT]
+    ) {
+      items {
+        timestamp
+        state
+        ... on DeviceConnectEvent {
+          session
+          txBytes
+          rxBytes
+          lifetime
+        }
+      }
+    }`
+
+export async function graphQLGetDeviceLogs(
+  id: string,
+  size: number,
+  after?: string,
+  minDate?: Date,
+  maxDate?: Date,
+  types?: IEventType[]
+) {
   return await graphQLBasicRequest(
-    `  query DeviceLogs($id: [String!]!, $after: ID, $size: Int, $minDate: DateTime, $maxDate: DateTime) {
+    `  query DeviceLogs($id: [String!]!, $after: ID, $size: Int, $minDate: DateTime, $maxDate: DateTime, $types: [EventType!]) {
           login {
             id
             device(id: $id) {  
@@ -73,13 +121,21 @@ export async function graphQLGetDeviceLogs(id: string, size: number, after?: str
       minDate: minDate?.toISOString(),
       maxDate: maxDate?.toISOString(),
       size,
+      types,
     }
   )
 }
 
-export async function graphQLGetLogs(account: string, size: number, after?: string, minDate?: Date, maxDate?: Date) {
+export async function graphQLGetLogs(
+  account: string,
+  size: number,
+  after?: string,
+  minDate?: Date,
+  maxDate?: Date,
+  types?: IEventType[]
+) {
   return await graphQLBasicRequest(
-    `  query Logs($account: String!, $after: ID, $size: Int, $minDate: DateTime, $maxDate: DateTime) {
+    `  query Logs($account: String!, $after: ID, $size: Int, $minDate: DateTime, $maxDate: DateTime, $types: [EventType!]) {
           login {
             account(id: $account) {
               id
@@ -94,13 +150,14 @@ export async function graphQLGetLogs(account: string, size: number, after?: stri
       minDate: minDate?.toISOString(),
       maxDate: maxDate?.toISOString(),
       size,
+      types,
     }
   )
 }
 
-export async function graphQLGetDeviceUrl(id: string, minDate?: Date, maxDate?: Date) {
+export async function graphQLGetDeviceUrl(id: string, minDate?: Date, maxDate?: Date, types?: IEventType[]) {
   return await graphQLBasicRequest(
-    `   query DeviceLogsUrl($id: [String!]!, $minDate: DateTime, $maxDate: DateTime) {
+    `   query DeviceLogsUrl($id: [String!]!, $minDate: DateTime, $maxDate: DateTime, $types: [EventType!]) {
           login {
             id
             device(id: $id) {  
@@ -114,13 +171,14 @@ export async function graphQLGetDeviceUrl(id: string, minDate?: Date, maxDate?: 
       id,
       minDate: minDate?.toISOString(),
       maxDate: maxDate?.toISOString(),
+      types,
     }
   )
 }
 
-export async function graphQLGetUrl(account: string, minDate?: Date, maxDate?: Date) {
+export async function graphQLGetUrl(account: string, minDate?: Date, maxDate?: Date, types?: IEventType[]) {
   return await graphQLBasicRequest(
-    `   query LogsUrl($account: String!, $minDate: DateTime, $maxDate: DateTime) {
+    `   query LogsUrl($account: String!, $minDate: DateTime, $maxDate: DateTime, $types: [EventType!]) {
           login {
             account(id: $account) {
               id
@@ -133,6 +191,62 @@ export async function graphQLGetUrl(account: string, minDate?: Date, maxDate?: D
       account,
       minDate: minDate?.toISOString(),
       maxDate: maxDate?.toISOString(),
+      types,
+    }
+  )
+}
+
+export async function graphQLGetConnectionUsage(
+  account: string,
+  size: number,
+  after?: string,
+  minDate?: Date,
+  maxDate?: Date
+) {
+  return await graphQLBasicRequest(
+    `  query ConnectionUsage($account: String!, $after: ID, $size: Int, $minDate: DateTime, $maxDate: DateTime) {
+          login {
+            account(id: $account) {
+              id
+              ${CONNECTION_USAGE_EVENTS}
+            }
+          }
+        }
+      `,
+    {
+      account,
+      after,
+      minDate: minDate?.toISOString(),
+      maxDate: maxDate?.toISOString(),
+      size,
+    }
+  )
+}
+
+export async function graphQLGetDeviceConnectionUsage(
+  id: string,
+  size: number,
+  after?: string,
+  minDate?: Date,
+  maxDate?: Date
+) {
+  return await graphQLBasicRequest(
+    `  query DeviceConnectionUsage($id: [String!]!, $after: ID, $size: Int, $minDate: DateTime, $maxDate: DateTime) {
+          login {
+            id
+            device(id: $id) {
+              id
+              ${DEVICE_CONNECTION_USAGE_EVENTS}
+            }
+          }
+        }
+      `,
+    {
+      id,
+      after,
+      minDate: minDate?.toISOString(),
+      maxDate: maxDate?.toISOString(),
+      size,
     }
   )
 }
