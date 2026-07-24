99b5373e032d28cf976e58bd551c2ff7432f3289
Jamie Ruderman <jamieruderman@gmail.com>
2026-07-24 11:06:49 -0700

    feat(i18n): extract device list chrome (header, empty state, action bar)

--- frontend/src/components/DevicesHeader.tsx
+++ frontend/src/components/DevicesHeader.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Container } from './Container'
 import { DevicesActionBars } from './DevicesActionBars'
 import { DevicesApplicationsTabs } from './DevicesApplicationsTabs'
+import { useTranslation } from 'react-i18next'
 import { Notice } from './Notice'
 import { Route } from 'react-router-dom'
 
 type Props = { select?: boolean; devices?: IDevice[]; children?: React.ReactNode }
 
 export const DevicesHeader: React.FC<Props> = ({ select, devices, children }) => {
+  const { t } = useTranslation()
   return (
     <Container
       integrated
@@ -18,7 +20,7 @@ export const DevicesHeader: React.FC<Props> = ({ select, devices, children }) =>
           <DevicesActionBars select={select} devices={devices} />
           <DevicesApplicationsTabs />
           <Route path="/devices/restore">
-            <Notice>Please select a device to restore.</Notice>
+            <Notice>{t('deviceList.selectToRestore', 'Please select a device to restore.')}</Notice>
           </Route>
         </>
       }
